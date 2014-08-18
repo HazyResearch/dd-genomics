29fe27182663e4c8ee6baa7dd7fe75f296f9e194
Matteo Riondato <matteo_riondato@brown.edu>
2014-08-18 12:42:08 -0700

    Add application configuration and scripts for setting environmental variables and for running the application.

---
+++ env.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+DIRNAME=`dirname $0`
+REAL_DIRNAME=`readlink -f ${DIRNAME}`
+
+export DEEPDIVE_HOME=`cd ${REAL_DIRNAME}/../..; pwd`
+export APP_HOME=`pwd`
+
+# Machine Configuration
+export MEMORY="4g"
+export PARALLELISM=4
+
+# Database Configuration
+export DBNAME=genomics
+export PGUSER=${PGUSER:-`whoami`}
+export PGPASSWORD=${PGPASSWORD:-}
+export PGPORT=${PGPORT:-5432}
+export PGHOST=${PGHOST:-localhost}
+
+# SBT Options
+export SBT_OPTS="-Xmx$MEMORY"
+
+# Using ddlib
+export PYTHONPATH=$DEEPDIVE_HOME/ddlib:$PYTHONPATH
+
---
+++ run.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+DIRNAME=`dirname $0`
+REAL_DIRNAME=`readlink -f ${DIRNAME}`
+
+. "${REAL_DIRNAME}/env.sh"
+
+cd $DEEPDIVE_HOME
+### Run with deepdive binary:
+#./deepdive -c $APP_HOME/application.conf
+### Compile and run:
+sbt "run -c $APP_HOME/application.conf"
