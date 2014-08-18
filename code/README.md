a340a2d582ad6a113c4cf668408819226f6090bf
Matteo Riondato <matteo_riondato@brown.edu>
2014-08-18 11:09:03 -0700

    Add scripts to load and clear database tables

--- code/README.md
+++ code/README.md
@@ -4,7 +4,11 @@ This directory contains most if not all the code.
 
 ## Files
 
+* `copy_table_from_file.sh`: Load a TSV file into a database table with the
+  PostgreSQL `COPY FROM` command.
 * `create_schema.sh`: Create the database schema.
+* `delete_from_table.sh`: Empty a database table with the PostgreSQL `DELETE
+  FROM` command.
 * `parser2sentences.py`: Convert parser output files into a TSV file that can be
   loaded in the `sentences` table with the PostgreSQL `COPY FROM` command.
 * `schema.sql`: SQL script to build the schema. Used in `create_schema.sh`.
---
+++ code/copy_table_from_file.sh
@@ -0,0 +1,24 @@
+#! /bin/sh
+#
+# Load a TSV file into a table using PostgreSQL COPY FROM command
+#
+# First argument is the database name
+# Second argument is the table name
+# Third argument is the path to the TSV file
+
+if [ $# -ne 3 ]; then
+	echo "$0: ERROR: wrong number of arguments" >&2
+	echo "$0: USAGE: $0 DB TABLE FILE" >&2
+	exit 1
+fi
+
+if [ ! -r $3 ]; then
+	echo "$0: ERROR: TSV file not readable" >&2
+	exit 1
+fi
+
+SQL_COMMAND_FILE=`mktemp /tmp/ctff.XXXXX` || exit 1
+echo "COPY $2 FROM '$3';" > ${SQL_COMMAND_FILE}
+psql -X -d $1 -f ${SQL_COMMAND_FILE} || exit 1
+rm ${SQL_COMMAND_FILE}
+
