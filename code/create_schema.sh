602718b6110ce323b1ba83e80e1d7af76fb30621
Matteo Riondato <matteo_riondato@brown.edu>
2014-08-18 11:51:00 -0700

    Fix path to schema file and call psql with ON_ERROR_STOP set, so we fail appropriately if we need to.

--- code/create_schema.sh
+++ code/create_schema.sh
@@ -11,11 +11,11 @@ if [ $# -ne 2 ]; then
 	exit 1
 fi
 
-SCHEMA_FILE="$1/data/schema.sql"
+SCHEMA_FILE="$1/code/schema.sql"
 if [ ! -r ${SCHEMA_FILE} ]; then
 	echo "$0: ERROR: schema file is not readable" >&2
 	exit 1
 fi
 
-psql -X -d $2 -f ${SCHEMA_FILE}
+psql -X --set ON_ERROR_STOP=1 -d $2 -f ${SCHEMA_FILE} 
 
