0053ef77b98e21428a49acd41eaec8e94f641a9f
Matteo Riondato <matteo_riondato@brown.edu>
2014-08-18 13:51:26 -0700

    Add schema for gene mentions table

--- code/schema.sql
+++ code/schema.sql
@@ -4,15 +4,40 @@
 -- Sentences table
 DROP TABLE IF EXISTS sentences CASCADE;
 CREATE TABLE sentences (
+	-- document id
 	docid text,
+	-- sentence id
 	sentid int,
+	-- word indexes
 	wordidxs int[],
+	-- words
 	words text[],
+	-- parts of speech
 	poses text[],
+	-- named entity recognition tags
 	ners text[],
+	-- lemmified version of words
 	lemmas text[],
+	-- dependency path labels
 	dep_paths text[],
+	-- dependency path parents
 	dep_parents int[],
+	-- bounding boxes
 	bounding_boxes text[]
+);
+
+-- Gene mentions
+DROP TABLE IF EXISTS gene_mentions CASCADE;
+CREATE TABLE gene_mentions (
+	-- id for random variable
+	id bigint,
+	-- mention id
+	mentionid text,
+	-- entity name
+	name text,
+	-- provenance
+	provenance text[],
+	-- is this a correct mention? (for training)
+	is_correct boolean
 );
 
