37d2329bf91f0741b0300e746e82c82482d61bcf
Matteo Riondato <matteo_riondato@brown.edu>
2014-08-19 09:07:00 -0700

    Rename script to be consistent.

--- code/schema.sql
+++ code/schema.sql
@@ -27,8 +27,8 @@ CREATE TABLE sentences (
 );
 
 -- Gene mentions
-DROP TABLE IF EXISTS gene_mentions CASCADE;
-CREATE TABLE gene_mentions (
+DROP TABLE IF EXISTS genes_mentions CASCADE;
+CREATE TABLE genes_mentions (
 	-- id for random variable
 	id bigint,
 	-- mention id
@@ -41,3 +41,23 @@ CREATE TABLE gene_mentions (
 	is_correct boolean
 );
 
+-- HPO terms mentions
+DROP TABLE IF EXISTS hpoterms_mentions CASCADE;
+CREATE TABLE hpoterms_mentions (
+	-- id for random variable
+	id bigint,
+	-- mention id
+	mentionid text,
+	-- entity name
+	name text,
+	-- provenance
+	provenance text[],
+	-- is this a correct mention? (for training)
+	is_correct boolean
+);
+
+-- 'gene <-> HPO term' relations
+DROP TABLE IF EXISTS gene_hpoterm_relations CASCADE;
+CREATE TABLE gene_hpoterm_relations (
+);
+        
