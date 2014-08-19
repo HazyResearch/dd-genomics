4a9ffa452b6f8c8f1fba735f6f618314e70612a6
Matteo Riondato <matteo_riondato@brown.edu>
2014-08-19 16:32:10 -0700

    Bring the mention extractors for the HPO terms to the new world order.

---
+++ code/extractor/MentionExtractor_HPOterm.py
@@ -0,0 +1,101 @@
+#! /usr/bin/env python3
+
+import random
+import sys
+import re
+
+from extractor.Extractor import MentionExtractor
+from dstruct.HPOtermMention import HPOtermMention
+from helper.easierlife import BASE_FOLDER, get_all_phrases_in_sentence
+
+HPOTERMS_DICT_FILENAME="/dicts/hpo_terms.tsv"
+
+NON_CORRECT_QUOTA = 100
+NON_CORRECT_PROBABILITY = 0.8
+
+# XXX (Matteo) This function should probably be in helper.easierlife
+# Given a list of words, return a list of variants built by splitting words
+# that contain the separator.
+# An example is more valuable:
+# let words = ["the", "cat/dog", "is", "mine"], the function would return ["the
+# cat is mine", "the doc is mine"]
+def get_variants(words, separator="/"):
+    if len(words) == 0:
+        return []
+    variants = []
+    base = []
+    i = 0
+    # Look for a word containing a "/"
+    while words[i].find(separator) == -1:
+        base.append(words[i])
+        i += 1
+        if i == len(words):
+            break
+    # If we found a word containing a "/", call recursively
+    if i < len(words):
+        variants_starting_words = words[i].split("/")
+        following_variants = get_variants(words[i+1:])
+        for variant_starting_word in variants_starting_words:
+            variant_base = base + [variant_starting_word]
+            if len(following_variants) > 0:
+                for following_variant in following_variants:
+                    variants.append(" ".join(variant_base +[following_variant]))
+            else:
+                variants.append(" ".join(variant_base))
+    else:
+        variants = [" ".join(base)]
+    return variants
+ 
+class MentionExtractor_HPOterm(MentionExtractor):
+    non_correct = 0
+
+    def __init__(self):
+        # Load the HPO terms dictionary
+        self.hpoterms_dict = dict()
+        self.max_variant_length = 0 # No. of words in longest variant
+        with open(BASE_FOLDER + HPOTERMS_DICT_FILENAME, 'rt') as hpoterms_dict_file:
+            for line in hpoterms_dict_file:
+                tokens = line.strip().split("\t")
+                # 1st token is name, 2nd is description, 3rd is 'C' and 4th is
+                # (presumably) the distance from the root of the DAG.
+                name = tokens[0]
+                description = tokens[1]
+                description_words = description.split()
+                variants = get_variants(description_words)
+                for variant in variants:
+                    if len(variant.split()) > self.max_variant_length:
+                        self.max_variant_length = len(variant.split())
+                    self.hpoterms_dict[variant.lower()] = name
+
+    def supervise(self):
+        pass
+
+    def extract(self, sentence):
+        history = set()
+        words = sentence.words
+        for start, end in get_all_phrases_in_sentence(sentence, self.max_variant_length):
+            if start in history or end in history:
+                    continue
+                
+            phrase = " ".join([word.word for word in words[start:end]])
+            phrase_lower = phrase.lower()
+
+            # Very simple rule: if the phrase is in the dictionary, then is a mention
+            if phrase_lower in self.hpoterms_dict:
+                # Found a mention with this start and end: we can insert the
+                # indexes of this mention in the history, and break the loop on
+                # end and get to a new start
+                for i in range(start, end + 1):
+                    history.add(i)
+                break
+                term = self.hpoterms_dict[phrase_lower]
+                mention = HPOtermMention(sentence.doc_id, term, words[start:end])
+                mention.is_correct = True
+                mention.add_features([]) # XXX TODO
+            elif self.non_correct < NON_CORRECT_QUOTA and random.random() < NON_CORRECT_PROBABILITY:
+                self.non_correct += 1
+                mention = HPOtermMention(sentence.doc_id, "NONCORRECT", words[start:end])
+                mention.is_correct = False
+                mention.add_features([]) # XXX TODO
+                yield mention
+
--- code/hpoterms_mentions_local.py
+++ code/hpoterms_mentions_local.py
@@ -1,108 +1,50 @@
 #! /usr/bin/env python3
 #
-#
-
+import fileinput
 import json
-import sys
+from extractor.MentionExtractor_HPOterm import MentionExtractor_HPOterm
+from dstruct.Sentence import Sentence
 
-# Type of the mentions
-MENTION_TYPE="HPOTERM"
+#MODE = "tsv"
+MODE = "json"
 
-# Number of non correct mentions to generate
-NON_CORRECT_QUOTA = 100
-# Probabily of generating a non correct mention
-NON_CORRECT_PROBABILITY = 0.7
+def TSVarray2list(_list):
+    # TODO (Matteo) implement this (should be in helper.easierlife)
+    return []
 
-# Given a list of words, return a list of variants built by splitting words
-# that contain the separator.
-# An example is more valuable:
-# let words = ["the", "cat/dog", "is", "mine"], the function would return ["the
-# cat is mine", "the doc is mine"]
-def get_variants(words, separator="/"):
-    if len(words) == 0:
-        return []
-    variants = []
-    base = []
-    i = 0
-    # Look for a word containing a "/"
-    while words[i].find(separator) == -1:
-        base.append(words[i])
-        i += 1
-        if i == len(words):
+def get_input_sentences(mode="tsv"):
+    for line in fileinput.input():
+        if mode == "tsv":
+            tokens = line.split("\t")
+            doc_id = tokens[0]
+            sent_id = int(tokens[1])
+            words = tokens[2]
+            poses = tokens[3]
+            ners = tokens[4]
+            lemmas = tokens[5]
+            dep_paths = tokens[6]
+            dep_parents = tokens[7]
+            bounding_boxes = tokens[8]
+            yield Sentence(doc_id, sent_id, TSVarray2list(words),
+                    TSVarray2list(poses), TSVarray2list(ners),
+                    TSVarray2list(lemmas), TSVarray2list(dep_paths),
+                    TSVarray2list(dep_parents), TSVarray2list(bounding_boxes))
+
+        elif mode == "json":
+            sent_dict = json.loads(line)
+            yield Sentence(sent_dict["doc_id"], sent_dict["sent_id"],
+                    sent_dict["words"], sent_dict["poses"], sent_dict["ners"],
+                    sent_dict["lemmas"], sent_dict["dep_paths"],
+                    sent_dict["dep_parents"], sent_dict["bounding_boxes"])
+        else:
             break
-    # If we found a word containing a "/", call recursively
-    if i < len(words):
-        variants_starting_words = words[i].split("/")
-        following_variants = get_variants(words[i+1:])
-        for variant_starting_word in variants_starting_words:
-            variant_base = base + [variant_starting_word]
-            if len(following_variants) > 0:
-                for following_variant in following_variants:
-                    variants.append(" ".join(variant_base +[following_variant]))
-            else:
-                variants.append(" ".join(variant_base))
-    else:
-        variants = [" ".join(base)]
-    return variants
 
+# Initialize the extractor
+mention_extractor = MentionExtractor_HPOterm()
 
-# Load the HPO terms dictionary
-HPOTERMS_DICT_FILENAME="/dicts/hpo_terms.tsv"
-hpoterms_dict = dict()
-max_variant_length = 0 # No. of words in longest variant
-with open(sys.argv[1] + HPOTERMS_DICT_FILENAME, 'rt') as hpoterms_dict_file:
-    for line in hpoterms_dict_file:
-        tokens = line.strip().split("\t")
-        # 1st token is name, 2nd is description, 3rd and 4th unknown 
-        # TODO (Matteo) ask Amir
-        name = tokens[0]
-        description = tokens[1]
-        description_words = description.split()
-        variants = get_variants(description_words)
-        for variant in variants:
-            if len(variant.split()) > max_variant_length:
-                max_variant_length = len(variant.split())
-            hpoterms_dict[variant.lower()] = name
-
-
-non_correct = 0
-# Process input
-for _row in sys.stdin:
-    row = json.loads(_row)
-    doc_id = row["docid"]
-    sent_id = row["sentid"]
-    wordidxs = row["wordidxs"]
-    words = row["words"]
-    poses = row["poses"]
-    ners = row["ners"]
-    lemmas = row["lemmas"]
-    dep_paths = row["dep_paths"]
-    dep_parents = row["dep_parents"]
-    bounding_boxes = row["bounding_boxes"]
-
-    history = set()
-    for start in range(len(words)):
-        for end in reversed(range(start + 1, min(len(words), start +
-            max_variant_length + 1))):
-
-            if start in history or end in history:
-                continue
-            
-            phrase = " ".join(words[start:end])
-            phrase_lower = phrase.lower()
-
-            # Very simple rule: if the word is in the dictionary, then is a mention
-            if phrase_lower in hpoterms_dict:
-                provenance = [ doc_id, sent_id, start, end - 1, phrase ]
-                mention_id = "_".join(str(x) for x in (doc_id, sent_id, start, end -1))
-                name = hpoterms_dict[phrase_lower]
-                print(json.dumps({"id": None, "mention_id": mention_id,
-                    "provenance": provenance, "name": name, "is_correct":
-                    True, "features": []}))
-                # Found a mention with this start and end: we can insert the
-                # indexes of this mention in the history, and break the loop on
-                # end and get to a new start
-                for i in range(start, end + 1):
-                    history.add(i)
-                break
+for sentence in get_input_sentences(MODE):
+    mention = mention_extractor.extract(sentence)
+    if mention != None:
+        mention.dump(MODE)
+    
 
