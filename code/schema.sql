dc2422d6d765fe68042e4726e9cdbafe4eabf723
Matteo Riondato <matteo_riondato@brown.edu>
2014-08-19 18:40:31 -0700

    New order for tables. Also comment out sentences so we don't recreate it.

--- code/schema.sql
+++ code/schema.sql
@@ -2,43 +2,51 @@
 -- WARNING: all the tables are dropped and recreated
 
 -- Sentences table
-DROP TABLE IF EXISTS sentences CASCADE;
-CREATE TABLE sentences (
-	-- document id
-	docid text,
-	-- sentence id
-	sentid int,
-	-- word indexes
-	wordidxs int[],
-	-- words
-	words text[],
-	-- parts of speech
-	poses text[],
-	-- named entity recognition tags
-	ners text[],
-	-- lemmified version of words
-	lemmas text[],
-	-- dependency path labels
-	dep_paths text[],
-	-- dependency path parents
-	dep_parents int[],
-	-- bounding boxes
-	bounding_boxes text[]
-);
+-- DROP TABLE IF EXISTS sentences CASCADE;
+-- CREATE TABLE sentences (
+-- 	-- document id
+-- 	doc_id text,
+-- 	-- sentence id
+-- 	sent_id int,
+-- 	-- word indexes
+-- 	wordidxs int[],
+-- 	-- words
+-- 	words text[],
+-- 	-- parts of speech
+-- 	poses text[],
+-- 	-- named entity recognition tags
+-- 	ners text[],
+-- 	-- lemmified version of words
+-- 	lemmas text[],
+-- 	-- dependency path labels
+-- 	dep_paths text[],
+-- 	-- dependency path parents
+-- 	dep_parents int[],
+-- 	-- bounding boxes
+-- 	bounding_boxes text[]
+-- );
 
 -- Gene mentions
 DROP TABLE IF EXISTS genes_mentions CASCADE;
 CREATE TABLE genes_mentions (
 	-- id for random variable
 	id bigint,
+	-- document id
+	doc_id text,
+	-- sentence id
+	sent_id int,
 	-- mention id
-	mentionid text,
-	-- entity name
-	name text,
-	-- provenance
-	provenance text[],
+	mention_id text,
+	-- start word id
+	start_word_id int,
+	-- end word id
+	end_word_id int,
+	-- mention type
+	type text,
 	-- is this a correct mention?
-	is_correct boolean
+	is_correct boolean,
+	-- string representation
+	repr text,
 	-- features for training
 	features text[]
 );
@@ -48,30 +56,23 @@ DROP TABLE IF EXISTS hpoterms_mentions CASCADE;
 CREATE TABLE hpoterms_mentions (
 	-- id for random variable
 	id bigint,
+	-- document id
+	doc_id text,
+	-- sentence id
+	sent_id int,
 	-- mention id
-	mentionid text,
-	-- entity name
-	name text,
-	-- provenance
-	provenance text[],
+	mention_id text,
+	-- start word id
+	start_word_id int,
+	-- end word id
+	end_word_id int,
+	-- mention type
+	type text,
 	-- is this a correct mention?
-	is_correct boolean
+	is_correct boolean,
+	-- string representation
+	repr text,
 	-- features for training
 	features text[]
 );
 
--- 'gene <-> HPO term' relations
-DROP TABLE IF EXISTS gene_hpoterm_relations CASCADE;
-CREATE TABLE gene_hpoterm_relations (
-	-- id for random variable
-	id bigint,
-	-- relation id
-	relationid text,
-	-- mention IDs composing this relation
-	mentions text[],
-	-- is this a correct relation?
-	is_correct boolean,
-	-- features for training
-	features text[]
-);
-        
