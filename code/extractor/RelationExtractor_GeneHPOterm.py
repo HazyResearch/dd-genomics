6b39b3a5122b970cf5934c970c29e38d99653cd4
Matteo Riondato <matteo_riondato@brown.edu>
2014-08-20 01:15:24 -0700

    Add some more features

--- code/extractor/RelationExtractor_GeneHPOterm.py
+++ code/extractor/RelationExtractor_GeneHPOterm.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 
+import re
 from Extractor import RelationExtractor
 from dstruct.RelationMention import RelationMention
 
@@ -11,13 +12,34 @@ class RelationExtractor_GeneHPOterm(RelationExtractor):
         pass
 
     def extract(self, sentence, gene, hpoterm):
-        rel = RelationMention("GENEHPOTERM", gene, hpoterm)
+        relation = RelationMention("GENEHPOTERM", gene, hpoterm)
 
-        self.supervise(sentence, gene, hpoterm, rel)
+        self.supervise(sentence, gene, hpoterm, relation)
 
         # Add features
-        # 
-        rel.add_features([sentence.dep_path(gene, hpoterm),])
+        # TODO (Matteo) Check those indices
+        gene_position = int(gene.id.split("_")[4])
+        hpoterm_position = int(hpoterm.id.split("_")[4])
+        start = min(gene_position, hpoterm_position)
+        end = max(gene_position, hpoterm_position)
+        sent_words_between = " ".join([w.lemma for w in sentence.words[start:end]])
 
-        return rel
+        # Verb between the two words, if present
+        for word in sent_words_between:
+                if re.search('VB\w*', word.pos):
+                        relation.add_features(["verb="+word.lemma])
+        # Word sequence between words
+        relation.add_features(["word_seq="+"_".join(sent_words_between)])
+        # Left and windows
+        if start > 0:
+            relation.add_features(["window_left_1={}".format(sentence.words[start-1])])
+        if end < len(sentence.words) - 1:
+            relation.add_features(["window_right_1={}".format(sentence.words[end])])
+
+        # Shortest dependency path between the two
+        gene_symbol = gene.symbol
+        hpoterm_term = hpoterm.name.lower()
+        relation.add_features([sentence.dep_path(gene_symbol, hpoterm_term),])
+
+        return relation
 
