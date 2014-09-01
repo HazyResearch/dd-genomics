38dd8b9b538b59d326a84bb17029eb63bcf7104b
Matteo Riondato <matteo_riondato@brown.edu>
2014-08-31 21:08:59 -0400

    Rename and make sure to add the gene which we know the geneRif contains. Add comments.

--- code/parser2geneRifs.py
+++ code/parser2geneRifs.py
@@ -1,7 +1,9 @@
 #! /usr/bin/env python3
 #
 # Take the json output of the NLPextractor extractor and convert it to TSV that
-# we can feed to the sentences table
+# we can feed to the database using COPY FROM. The schema of the table is equal
+# to the 'sentences' table except for an additional column at the end which is
+# the gene that we know the geneRif contains.
 
 import fileinput
 import json
@@ -11,13 +13,20 @@ from parser2sentences import list2TSVarray
 
 
 if len(sys.argv) < 2:
-    sys.stderr.write("USAGE: {} FILE [FILE [FILE [...]]]\n".format(sys.argv[0]))
+    sys.stderr.write("USAGE: {} GENE_RIFS_DICT PARSER_OUTPUT [PARSER_OUTPUT [PARSER_OUTPUT [...]]]\n".format(sys.argv[0]))
     sys.exit(1)
 
-with fileinput.input() as input_files:
+genes = []
+with open(sys.argv[1], 'rt') as gene_rifs_dict_file:
+    for line in gene_rifs_dict_file:
+        tokens = line.strip().split("\t")
+        genes.append(tokens[1])
+
+line_index = 0
+with fileinput.input(sys.argv[2:]) as input_files:
     for line in input_files:
         line_dict = json.loads(line)
-        docid = line_dict["doc_id"]
+        doc_id = line_dict["doc_id"]
         sent_id = line_dict["sent_id"]
         words = line_dict["words"]
         wordidxs = [x for x in range(len(words))]
@@ -27,11 +36,9 @@ with fileinput.input() as input_files:
         dep_paths_orig = line_dict["dep_paths"]
         bounding_boxes = [""] * len(words)
 
+        # Compute dependency path edge labels and node parents
         dep_paths = ["_"] * len(words)
         dep_parents = [0] * len(words)
-        assert len(words) == len(poses)
-        assert len(words) == len(ners)
-        assert len(words) == len(lemmas)
         for dep_path in dep_paths_orig:
             tokens = dep_path.split("(")
             dep_parent = int((tokens[1].split(",")[0]).split("-")[1]) - 1
@@ -39,11 +46,12 @@ with fileinput.input() as input_files:
             dep_paths[dep_child] = tokens[0]
             dep_parents[dep_child] = dep_parent
 
-        print("{}\n".format("\t".join([docid, str(sent_id),
+        print("{}\n".format("\t".join([doc_id, str(sent_id),
             list2TSVarray(wordidxs), list2TSVarray(words,
                 quote=True), list2TSVarray(poses, quote=True),
             list2TSVarray(ners), list2TSVarray(lemmas, quote=True),
             list2TSVarray(dep_paths, quote=True),
             list2TSVarray(dep_parents),
-            list2TSVarray(bounding_boxes)])))
+            list2TSVarray(bounding_boxes), genes[line_index]])))
+        line_index += 1
 
