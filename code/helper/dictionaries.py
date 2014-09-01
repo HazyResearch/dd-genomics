7a5d86013ef415a71fb0007b91861d78c8fa1a87
Matteo Riondato <matteo_riondato@brown.edu>
2014-09-01 15:05:25 -0700

    Fix small bugs

--- code/helper/dictionaries.py
+++ code/helper/dictionaries.py
@@ -16,11 +16,11 @@ def load_merged_genes_dictionary(filename):
     merged_genes_dict = dict()
     with open(filename, 'rt') as merged_genes_dict_file:
         for line in merged_genes_dict_file:
-            tokens = line.strip().split("\t")
+            tokens = line[:-1].split("\t")
             symbol = tokens[0]
             alternate_symbols = tokens[1].split("|")
             names = tokens[2].split("|")
-            for sym in [symbol] + alternate_symbols + names:
+            for sym in [symbol,] + alternate_symbols + names:
                 if sym.casefold() not in merged_genes_dict:
                     merged_genes_dict[sym.casefold()] = []
                 merged_genes_dict[sym.casefold()].append(symbol)
