17a4ecf35287ebd9d3250b71edfa98fa84640c9e
Matteo Riondato <matteo_riondato@brown.edu>
2014-09-04 00:43:11 -0700

    Rework logic for "Abbreviations" line, to fix bug that was only parsing the first abbreviation. Skip if the definition is in the gene dictionary (+ pep8ify)

--- code/find_acronyms.py
+++ code/find_acronyms.py
@@ -3,18 +3,17 @@
 # Look for acronyms defined in the sentence that look like gene symbols
 
 import fileinput
-import json
-
 from dstruct.Sentence import Sentence
 from helper.dictionaries import load_dict
 from helper.easierlife import get_dict_from_TSVline, no_op, TSVstring2list
+
 
 # Return acronyms from sentence
 def extract(sentence):
     acronyms = []
     # First method: Look for a sentence that starts with "Abbreviations"
-    if len(sentence.words) > 0 and sentence.words[0].word == "Abbreviations":
-        index = 2
+    if len(sentence.words) > 0 and sentence.words[0].word.casefold() == "abbreviations":
+        index = [x.word for x in sentence.words].index(":") + 1
         while index < len(sentence.words):
             acronym = dict()
             acronym["acronym"] = sentence.words[index].word
@@ -22,27 +21,30 @@ def extract(sentence):
                 definition_end = sentence.words.index(";", index + 1)
             except:
                 definition_end = len(sentence.words) - 1
-            # Skip acronym if it's not in the genes dictionary
-            if sentence.words[index].word.casefold() not in merged_genes_dict:
+            definition = " ".join([x.word for x in
+                                   sentence.words[index + 1:definition_end]])
+            # Skip acronym if it's not in the genes dictionary or if the
+            # definition is in it.
+            if sentence.words[index].word.casefold() not in merged_genes_dict \
+                    or definition.casefold() in merged_genes_dict:
                 index = definition_end + 1
                 continue
             acronym["doc_id"] = sentence.doc_id
             acronym["sent_id"] = sentence.sent_id
             acronym["word_idx"] = sentence.words[index].in_sent_idx
-            acronym["definition"] = " ".join([x.word for x in
-                sentence.words[index + 1:definition_end]])
+            acronym["definition"] = definition
             acronyms.append(acronym)
             index = definition_end + 1
-    else: 
+    else:
         # Second method: find 'A Better Example (ABE)' type of definitions.
         # Scan all the words in the sentence
         for word in sentence.words:
             if word.word.casefold() not in merged_genes_dict:
                 continue
             acronym = None
-            # Look for definition only if this word has length at least 2 and is
-            # all capitals and it comes between "(" and ")" or "(" and ";" or "("
-            # and ","
+            # Look for definition only if this word has length at least 2 and
+            # is all capitals and it comes between "(" and ")" or "(" and ";"
+            # or "(" and ","
             if word.word.isalpha() and len(word.word) >= 2 and word.word.isupper() \
             and word.in_sent_idx > 0 and word.in_sent_idx < len(sentence.words) - 1 \
             and sentence.words[word.in_sent_idx - 1].word == "(" \
@@ -59,13 +61,14 @@ def extract(sentence):
                         if window_words[window_index].word[0].lower() != word.word[window_index].lower():
                             is_definition = False
                             break
-                    if is_definition:
+                    definition = " ".join([w.word for w in window_words]) 
+                    if is_definition and definition.casefold() not in merged_genes_dict:
                         acronym = dict()
                         acronym["doc_id"] = word.doc_id
                         acronym["sent_id"] = word.sent_id
                         acronym["word_idx"] = word.in_sent_idx
                         acronym["acronym"] = word.word
-                        acronym["definition"] = " ".join([w.word for w in window_words])
+                        acronym["definition"] = definition
                         acronyms.append(acronym)
                         break
                     start_idx += 1
