5dd9475b115386343cf499621da70e261219cfdd
Matteo Riondato <matteo_riondato@brown.edu>
2014-09-06 22:31:55 -0700

    Add some features

--- code/extract_hpoterm_mentions.py
+++ code/extract_hpoterm_mentions.py
@@ -2,6 +2,7 @@
 
 import fileinput
 import random
+import re
 
 from dstruct.Mention import Mention
 from dstruct.Sentence import Sentence
@@ -14,6 +15,14 @@ SUPERVISION_PROB = 0.5
 RANDOM_EXAMPLES_PROB = 0.01
 RANDOM_EXAMPLES_QUOTA = 7500
 random_examples = 0
+
+HPOTERM_KEYWORDS = frozenset(
+    ["syndrome", "gene", "association", "apoptosis", "genotype", "disease",
+        "cancer", "carcinoma", "abnormality", "mutation", "protein",
+        "diagnose", "patient", "patients", "viruses", "virus", "therapy",
+        "symptom", "cronic,", "diagnosis", "detection", "severe", "phenotype",
+        "affect", "genome", "genomic", "therapeutic", "pathway", "injury",
+        "chromosome", "deletion", "polymorphism"])
 
 
 # Perform the supervision
@@ -29,25 +38,80 @@ def supervise(mention, sentence):
 def add_features(mention, sentence):
     # The word "two on the left" of the mention, if present
     if mention.wordidxs[0] > 1:
-        mention.add_feature("WINDOW_LEFT_2_[{}]".format(sentence.words[mention.wordidxs[0] - 2].lemma))
+        mention.add_feature("WINDOW_LEFT_2_[{}]".format(
+            sentence.words[mention.wordidxs[0] - 2].lemma))
     # The word "two on the right" on the left of the mention, if present
     if mention.wordidxs[-1] + 2 < len(sentence.words):
-        mention.add_feature("WINDOW_RIGHT_2_[{}]".format(sentence.words[mention.wordidxs[-1] + 2].lemma))
+        mention.add_feature("WINDOW_RIGHT_2_[{}]".format(
+            sentence.words[mention.wordidxs[-1] + 2].lemma))
     # The word on the left of the mention, if present
     if mention.wordidxs[0] > 0:
-        mention.add_feature("WINDOW_LEFT_1_[{}]".format(sentence.words[mention.wordidxs[0] - 1].lemma))
+        mention.add_feature("WINDOW_LEFT_1_[{}]".format(
+            sentence.words[mention.wordidxs[0] - 1].lemma))
     # The word on the right of the mention, if present
     if mention.wordidxs[-1] + 1 < len(sentence.words):
-        mention.add_feature("WINDOW_RIGHT_1_[{}]".format(sentence.words[mention.wordidxs[-1] + 1].lemma))
+        mention.add_feature("WINDOW_RIGHT_1_[{}]".format(
+            sentence.words[mention.wordidxs[-1] + 1].lemma))
+    # The keywords that appear in the sentence with the mention
+    minl = 100
+    minp = None
+    minw = None
+    for word2 in sentence.words:
+        if word2.lemma in HPOTERM_KEYWORDS:
+            p = sentence.get_word_dep_path(mention.wordidxs[0],
+                                           word2.in_sent_idx)
+            mention.add_feature("KEYWORD_[" + word2.lemma + "]")
+            if len(p) < minl:
+                minl = len(p)
+                minp = p
+                minw = word2.lemma
+    # Special feature for the keyword on the shortest dependency path
+    if minw:
+        mention.add_feature('EXT_KEYWORD_SHORTEST_PATH_[' + minw + ']' + minp)
+        mention.add_feature('KEYWORD_SHORTEST_PATH_[' + minw + ']')
+    # The labels and the NERs on the shortest dependency path
+    # between a verb and the mention word.
+    minl = 100
+    minp = None
+    minw = None
+    for word2 in sentence.words:
+        if re.search('^VB[A-Z]*$', word2.pos) and word2.lemma != 'be':
+            p = sentence.get_word_dep_path(mention.wordidxs[0],
+                                           word2.in_sent_idx)
+            if len(p) < minl:
+                minl = len(p)
+                minp = p
+                minw = word2.lemma
+    if minw:
+        mention.add_feature('EXT_VERB_PATH_[' + minw + ']' + minp)
+        mention.add_feature('VERB_PATH_[' + minw + ']')
+    # There is no verb in the sentence
+    # This may be useful to push down weird/junk sentences
+    no_verb = True
+    for word in sentence.words:
+        if re.search('^VB[A-Z]*$', word.pos):
+            no_verb = False
+            break
+    if no_verb:
+        mention.add_feature("NO_VERB_IN_SENTENCE")
 
 
-## Return a list of mentions from the sentence
+# Add features that are related to the entire set of mentions candidates
+def add_features_to_all(mentions, sentence):
+    # Number of other mentions in the sentence
+    for i in range(1, len(mentions)):
+        for mention in mentions:
+            mention.add_feature("{}_OTHER_MENTIONS_IN_SENTENCE".format(i))
+
+
+# Return a list of mentions from the sentence
 def extract(sentence):
     global random_examples
     mentions = []
     history = set()
     words = sentence.words
-    for start, end in get_all_phrases_in_sentence(sentence, max_variant_length):
+    for start, end in get_all_phrases_in_sentence(
+            sentence, max_variant_length):
         if start in history or end in history:
                 continue
         phrase = " ".join([word.word for word in words[start:end]])
@@ -67,9 +131,11 @@ def extract(sentence):
             mentions.append(mention)
         else:
             # Potentially generate a random mention that resembles real ones
-            # This mention is supervised (as false) in the code calling this function
+            # This mention is supervised (as false) in the code calling this
+            # function
             # XXX (Matteo) may need additional conditions to generate a mention
-            if random.random() < RANDOM_EXAMPLES_PROB and random_examples < RANDOM_EXAMPLES_QUOTA:
+            if random.random() < RANDOM_EXAMPLES_PROB and \
+                    random_examples < RANDOM_EXAMPLES_QUOTA:
                 mention = Mention("RANDOM", "random", words[start:end])
                 random_examples += 1
                 # Add features
@@ -82,12 +148,13 @@ def extract(sentence):
 
 # Load the dictionaries that we need
 hpoterms_dict = load_dict("hpoterms")
-# Create supervision dictionary that only contains a fraction of the genes in the gene
-# dictionary. This is to avoid that we label as positive examples everything
-# that is in the dictionary
+# Create supervision dictionary that only contains a fraction of the genes in
+# the gene dictionary. This is to avoid that we label as positive examples
+# everything that is in the dictionary
 supervision_hpoterms_dict = dict()
-to_sample = set(random.sample(range(len(hpoterms_dict)),
-        int(len(hpoterms_dict) * SUPERVISION_HPOTERMS_DICT_FRACTION)))
+to_sample = set(random.sample(range(
+    len(hpoterms_dict)), int(
+        len(hpoterms_dict) * SUPERVISION_HPOTERMS_DICT_FRACTION)))
 i = 0
 for hpoterm in hpoterms_dict:
     if i in to_sample:
@@ -104,30 +171,26 @@ if __name__ == "__main__":
     # Process the input
     with fileinput.input() as input_files:
         for line in input_files:
-            line_dict = get_dict_from_TSVline(line, 
-                                              ["doc_id", "sent_id", "wordidxs",
-                                                  "words", "poses", "ners",
-                                                  "lemmas", "dep_paths",
-                                                  "dep_parents",
-                                                  "bounding_boxes"], 
-                                              [no_op, int, lambda x:
-                                                  TSVstring2list(x, int),
-                                                  TSVstring2list,
-                                                  TSVstring2list,
-                                                  TSVstring2list,
-                                                  TSVstring2list,
-                                                  TSVstring2list, lambda x:
-                                                  TSVstring2list(x, int),
-                                                  TSVstring2list])
-            sentence = Sentence(line_dict["doc_id"], line_dict["sent_id"],
-                                line_dict["wordidxs"], line_dict["words"],
-                                line_dict["poses"], line_dict["ners"],
-                                line_dict["lemmas"], line_dict["dep_paths"],
-                                line_dict["dep_parents"],
-                                line_dict["bounding_boxes"])
+            line_dict = get_dict_from_TSVline(
+                line,
+                ["doc_id", "sent_id", "wordidxs", "words", "poses", "ners",
+                    "lemmas", "dep_paths", "dep_parents", "bounding_boxes"],
+                [no_op, int, lambda x: TSVstring2list(x, int), TSVstring2list,
+                    TSVstring2list, TSVstring2list, TSVstring2list,
+                    TSVstring2list, lambda x: TSVstring2list(x, int),
+                    TSVstring2list])
+            sentence = Sentence(
+                line_dict["doc_id"], line_dict["sent_id"],
+                line_dict["wordidxs"], line_dict["words"], line_dict["poses"],
+                line_dict["ners"], line_dict["lemmas"], line_dict["dep_paths"],
+                line_dict["dep_parents"], line_dict["bounding_boxes"])
+            mentions = extract(sentence)
+            if len(mentions) > 1:
+                add_features_to_all(mentions, sentence)
             for mention in extract(sentence):
                 if mention.type != "RANDOM":
                     supervise(mention, sentence)
                 else:
+                    mention.add_feature("IS_RANDOM")
                     mention.is_correct = False
                 print(mention.tsv_dump())
