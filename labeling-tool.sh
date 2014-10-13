448eaeeb4c88159715a80395cab564585c352b23
Jaeho Shin <netj@sparcs.org>
2014-10-13 04:43:00 -0700

    Example data and script for labeling with Mindtagger

---
+++ labeling-tool.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# A script to start Mindtagger for tasks under labeling/
+# Author: Jaeho Shin <netj@cs.stanford.edu>
+# Created: 2014-10-11
+set -eu
+
+# set up environment to run Mindtagger
+cd "$(dirname "$0")"
+PATH="$PWD/bin:$PATH"
+
+# install Mindbender locally if not available
+if ! type mindbender &>/dev/null; then
+    release=${MINDBENDER_RELEASE:=LATEST}
+    tool=bin/mindbender
+    mkdir -p bin
+    echo >&2 "Downloading Mindbender..."
+    curl --show-error --output $tool.download \
+        https://github.com/netj/mindbender/releases/download/$release/mindbender-$release-$(uname)-$(uname -m).sh
+    chmod +x $tool.download
+    mv -f $tool.download $tool
+fi
+
+# start Mindtagger for all tasks available under labeling/
+shopt -s globstar 2>/dev/null || true
+mindbender tagger labeling/**/mindtagger.conf
