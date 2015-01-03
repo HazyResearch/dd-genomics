9ff2665626da410849a3af04333b0fbdb9677495
Matteo Riondato <matteo_riondato@brown.edu>
2015-01-02 21:07:52 -0800

    Fix recall script

--- labeling/scripts/gp_relations_recall.sql
+++ labeling/scripts/gp_relations_recall.sql
@@ -23,8 +23,10 @@ AND
 AND 
 	t0.mention_id_1 = t5.mention_id AND t0.mention_id_2 = t6.mention_id
 AND
-	t0.expectation > 0.2
+	t0.is_correct is not False
+--AND
+--t0.expectation > 0.2
 ORDER BY random()
-LIMIT 400
+LIMIT 1000
 ) TO STDOUT WITH HEADER
 ;
