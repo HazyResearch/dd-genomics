857f41604a4f702a5adf0609c909b0680be9d120
Robin Jia <robinjia@stanford.edu>
2015-04-15 01:39:08 -0700

    Adds very basic gene recall mindtagger template

---
+++ labeling/templates/gene-recall/input.sql
@@ -0,0 +1,18 @@
+COPY (
+  SELECT si.doc_id
+    , si.sent_id
+    , gm.mention_id
+    , gm.type
+    , string_to_array(si.words, '|^|') as words
+    , gm.entity
+    , expectation
+    , gm.wordidxs
+  FROM gene_mentions_is_correct_inference gmi
+    , sentences_input si
+    , gene_mentions gm
+  WHERE si.doc_id = gm.doc_id
+    AND si.sent_id = gm.sent_id
+    AND gm.mention_id = gmi.mention_id
+    AND expectation <= 0.9
+  ORDER BY random() LIMIT 100
+) TO STDOUT WITH CSV HEADER;
