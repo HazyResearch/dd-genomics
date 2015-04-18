9cf712421ccf03d9497e54e436921eda1f14ec82
Robin Jia <robinjia@stanford.edu>
2015-04-17 17:33:40 -0700

    Adds most influential features to mindtagger

--- labeling/templates/gene-recall/input.sql
+++ labeling/templates/gene-recall/input.sql
@@ -7,12 +7,25 @@ COPY (
     , gm.entity
     , expectation
     , gm.wordidxs
+    , features
+    , weights
   FROM gene_mentions_is_correct_inference gmi
     , sentences_input si
     , gene_mentions gm
+    , ( -- find features relevant to the relation
+       SELECT mention_id
+            , ARRAY_AGG(feature ORDER BY abs(weight) DESC) AS features
+            , ARRAY_AGG(weight  ORDER BY abs(weight) DESC) AS weights
+         FROM gene_features f
+            , dd_inference_result_weights_mapping wm
+        WHERE wm.description = ('gene_inference-' || f.feature)
+          AND abs(weight) >= 1
+        GROUP BY mention_id
+      ) f
   WHERE si.doc_id = gm.doc_id
     AND si.sent_id = gm.sent_id
     AND gm.mention_id = gmi.mention_id
+    and gm.mention_id = f.mention_id
     AND expectation <= 0.9
   ORDER BY random() LIMIT 100
 ) TO STDOUT WITH CSV HEADER;
--- labeling/templates/pheno-precision/input.sql
+++ labeling/templates/pheno-precision/input.sql
@@ -7,12 +7,25 @@ COPY (
     , pm.entity
     , expectation
     , pm.wordidxs
+    , features
+    , weights
   FROM pheno_mentions_is_correct_inference pmi
     , sentences_input si
     , pheno_mentions pm
+    , ( -- find features relevant to the relation
+       SELECT mention_id
+            , ARRAY_AGG(feature ORDER BY abs(weight) DESC) AS features
+            , ARRAY_AGG(weight  ORDER BY abs(weight) DESC) AS weights
+         FROM pheno_features f
+            , dd_inference_result_weights_mapping wm
+        WHERE wm.description = ('pheno_inference-' || f.feature)
+          AND abs(weight) >= 1
+        GROUP BY mention_id
+      ) f
   WHERE si.doc_id = pm.doc_id
     AND si.sent_id = pm.sent_id
     AND pm.mention_id = pmi.mention_id
+    AND pm.mention_id = f.mention_id
     AND expectation > 0.9
   ORDER BY random() LIMIT 100
 ) TO STDOUT WITH CSV HEADER;
