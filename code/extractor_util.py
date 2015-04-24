be0f61f929ff721c2b824be9d4f4b2d8ed33b319
Robin Jia <robinjia@stanford.edu>
2015-04-23 18:06:18 -0700

    Adds HPO DAG python utilities

--- code/extractor_util.py
+++ code/extractor_util.py
@@ -1,5 +1,5 @@
 """Miscellaneous shared tools for extractors."""
-from collections import namedtuple
+from collections import defaultdict, namedtuple
 import os
 
 CODE_DIR = os.path.dirname(os.path.realpath(__file__))
@@ -13,6 +13,70 @@ Sentence = namedtuple(
 Mention = namedtuple(
     'Mention', ['dd_id', 'doc_id', 'sent_id', 'wordidxs', 'mention_id',
                 'mention_type', 'entity', 'words', 'is_correct'])
+
+
+class Dag:
+  """Class representing a directed acyclic graph."""
+  def __init__(self, nodes, edges):
+    self.nodes = nodes
+    self.node_set = set(nodes)
+    self.edges = edges  # edges is dict mapping child to list of parents
+    self._has_child_memoizer = defaultdict(dict)
+
+  def has_child(self, parent, child):
+    """Check if child is a child of parent."""
+    if child not in self.node_set:
+      raise ValueError('"%s" not in the DAG.' % child)
+    if parent not in self.node_set:
+      raise ValueError('"%s" not in the DAG.' % parent)
+    if child == parent:
+      return True
+    if child in self._has_child_memoizer[parent]:
+      return self._has_child_memoizer[parent][child]
+    for node in self.edges[child]:
+      if self.has_child(parent, node):
+        self._has_child_memoizer[parent][child] = True
+        return True
+    self._has_child_memoizer[parent][child] = False
+    return False
+
+
+def read_hpo_dag():
+  with open('%s/onto/data/hpo_phenotypes.tsv' % APP_HOME) as f:
+    nodes = []
+    edges = {}
+    for line in f:
+      toks = line.strip(' \r\n').split('\t')
+      child = toks[0]
+      nodes.append(child)
+      parents_str = toks[5]
+      if parents_str:
+        edges[child] = parents_str.split('|')
+      else:
+        edges[child] = []
+    return Dag(nodes, edges)
+
+
+def get_hpo_phenos(hpo_dag, parent='HP:0000118'):
+  """Get only the children of 'Phenotypic Abnormality' (HP:0000118)."""
+  return [hpo_term for hpo_term in hpo_dag.nodes
+          if hpo_dag.has_child(parent, hpo_term)]
+
+
+
+def read_hpo_synonyms():
+  syn_dict = dict()
+  with open('%s/onto/data/hpo_phenotypes.tsv' % APP_HOME) as f:
+    for line in f:
+      toks = line.strip(' \r\n').split('\t')
+      node = toks[0]
+      syn_dict[node] = node
+      syn_str = toks[4]
+      if syn_str:
+        print syn_str
+        for syn in syn_str.split('|'):
+          syn_dict[syn] = node
+  return syn_dict
 
 
 def create_mention(sentence, wordidxs, words, entity, mention_type=None, is_correct=None):
