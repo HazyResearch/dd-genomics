1a97c14b1b5e9474dbaa42fc901f6247c84cece2
awenger <aaron.m.wenger@gmail.com>
2015-04-28 17:40:00 -0700

    Give DD_DBNAME precedence over DBNAME

--- util/create_schema.sh
+++ util/create_schema.sh
@@ -9,8 +9,8 @@ if [ "${GDD_HOME-}" == "" ]; then
     echo "Set GDD_HOME to your Genomics DeepDive home directory."
     exit 1
 fi
-if [ "${DBNAME-}" == "" ]; then
-    echo "Set DBNAME to your Genomics DeepDive database name."
+if [ "${DD_DBNAME-${DBNAME-}}" == "" ]; then
+    echo "Set DD_DBNAME to your Genomics DeepDive database name."
     exit 1
 fi
 
@@ -19,4 +19,4 @@ if [ ! -r ${SCHEMA_FILE} ]; then
     echo "$0: ERROR: schema file is not readable" >&2
     exit 1
 fi
-sed 's@DISTRIBUTED BY .*;@;@g' ${SCHEMA_FILE} | psql -q -X --set ON_ERROR_STOP=1 -d ${DBNAME} 
+sed 's@DISTRIBUTED BY .*;@;@g' ${SCHEMA_FILE} | psql -q -X --set ON_ERROR_STOP=1 -d ${DD_DBNAME-${DBNAME-}}
