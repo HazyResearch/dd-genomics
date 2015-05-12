585eaf0601b50a281ec735a74f7ed828087e5e9b
Robin Jia <robinjia@stanford.edu>
2015-05-12 16:53:55 -0700

    More updates to pheno-recall mindtagger task

--- labeling/templates/pheno-recall/input.sql
+++ labeling/templates/pheno-recall/input.sql
@@ -6,13 +6,13 @@ COPY (
     , pm.wordidxs
   FROM (
       SELECT si.doc_id as doc_id
-            , si.sent_id as sent_id
-            , string_to_array(si.words, '|^|') as words
-            , mesh.hpo_id as entity
+        , si.sent_id as sent_id
+        , string_to_array(si.words, '|^|') as words
+        , mesh.hpo_id as entity
       FROM sentences_input si
-          , hpo_to_doc_via_mesh mesh
-      WHERE si.doc_id = mesh.doc_id) t
+        , (SELECT * FROM hpo_to_doc_via_mesh ORDER BY random() LIMIT 100) mesh
+      WHERE
+        si.doc_id = mesh.doc_id) t
     LEFT OUTER JOIN pheno_mentions pm
     ON t.doc_id = pm.doc_id AND t.sent_id = pm.sent_id AND t.entity = pm.entity
-  ORDER BY random() LIMIT 100
 ) TO STDOUT WITH CSV HEADER;
