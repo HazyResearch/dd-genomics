7cfee78505484d4613b8c36b4b223253886eb588
Alexander Ratner <ajratner@raiders2.stanford.edu>
2015-05-28 21:42:36 -0700

    Minor demo with Morgan- adding new feature

--- code/genepheno_extract_features.py
+++ code/genepheno_extract_features.py
@@ -20,17 +20,25 @@ parser = util.RowParser([
           ('dep_paths', 'text[]'),
           ('dep_parents', 'int[]'),
           ('wordidxs', 'int[]')])
+
+Feature = namedtuple('Feature', ['doc_id', 'relation_id', 'name'])
           
 def get_features_for_candidate(row):
   """Extract features for candidate mention- both generic ones from ddlib & custom features"""
   features = []
+  f = Feature(doc_id=row.doc_id, relation_id=row.relation_id, name=None)
   dds = util.create_ddlib_sentence(row)
 
   # (1) GENERIC FEATURES from ddlib
   gene_span = ddlib.Span(begin_word_id=row.gene_wordidxs[0], length=len(row.gene_wordidxs))
   pheno_span = ddlib.Span(begin_word_id=row.pheno_wordidxs[0], length=len(row.pheno_wordidxs))
-  features += [(row.doc_id, row.relation_id, feat) \
+  features += [f._replace(name=feat) \
                     for feat in ddlib.get_generic_features_relation(dds, gene_span, pheno_span)]
+
+  # (2) Are the gene and pheno mentions directly adjacent?
+  # TODO: buckets for distance between them > 0?
+  if row.gene_wordidxs[-1] == row.pheno_wordidxs[0] - 1 or row.gene_wordidxs[0] == row.pheno_wordidxs[-1] + 1:
+    features.append(f._replace(name='G_P_ADJACENT'))
   return features
 
 # Helper for loading in manually defined keywords
