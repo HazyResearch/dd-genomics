c048ea0d9c5c5f15aa51d844e92fcfd7a325cb2b
Alexander Ratner <ajratner@raiders2.stanford.edu>
2015-05-28 20:26:37 -0700

    Working on repairing inference rules

--- util/input_schema.sql
+++ util/input_schema.sql
@@ -52,55 +52,6 @@ CREATE TABLE sentences_input (
  	bounding_boxes text
  ) DISTRIBUTED BY (doc_id);
 
--- GeneRifs table
-DROP TABLE IF EXISTS generifs CASCADE;
-CREATE TABLE generifs (
-	-- document id
-	doc_id text,
-	-- sentence id
-	sent_id int,
-	-- word indexes
-	wordidxs int[],
-	-- words
-	words text[],
-	-- parts of speech
-	poses text[],
-	-- named entity recognition tags
-	ners text[],
-	-- lemmified version of words
-	lemmas text[],
-	-- dependency path labels
-	dep_paths text[],
-	-- dependency path parents
-	dep_parents int[],
-	-- bounding boxes
-	bounding_boxes text[],
-	-- 'labelled' gene that is contained in the geneRif
-	gene text
-) DISTRIBUTED BY (doc_id);
-
--- HPO phenotype DAG
-DROP TABLE IF EXISTS hpo_dag CASCADE;
-CREATE TABLE hpo_dag (
-  -- parent HPO ID
-  parent text,
-  -- child HPO ID
-  child text,
-  -- Is 'child' a child of 'Phenotypic Abnormality' (HP:0000118)
-  is_pheno boolean
-);
-
--- Reactome pathways
-DROP TABLE IF EXISTS reactome_pathways CASCADE;
-CREATE TABLE reactome_pathways (
-  -- pathway (Reactome ID)
-  pathway_id text,
-  -- gene (HGNC symbol)
-  gene text,
-  -- pathway name
-  pathway_name text
-);
-
 -- Distantly supervision via MeSH: HPO to document map
 DROP TABLE IF EXISTS hpo_to_doc_via_mesh CASCADE;
 CREATE TABLE hpo_to_doc_via_mesh (
