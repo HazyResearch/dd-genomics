2f5c9a5194a54861bb3fb9e793168d4ed9134072
Alexander Ratner <ajratner@raiders2.stanford.edu>
2015-07-14 21:25:40 -0700

    Removed unused: entailment stuff, Dockerfile

--- entailment/scripts/stats.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source ../env_local.sh
-true_pos=`psql -X --set ON_ERROR_STOP=1 -d ${DBNAME} -c 'COPY (SELECT COUNT(*) FROM genepheno_facts_is_correct_inference WHERE expectation > 0.9 AND is_correct = true) TO STDOUT'`
-false_pos=`psql -X --set ON_ERROR_STOP=1 -d ${DBNAME} -c 'COPY (SELECT COUNT(*) FROM genepheno_facts_is_correct_inference WHERE expectation > 0.9 AND is_correct <> true) TO STDOUT'`
-false_neg=`psql -X --set ON_ERROR_STOP=1 -d ${DBNAME} -c 'COPY (SELECT COUNT(*) FROM genepheno_facts_is_correct_inference WHERE expectation <= 0.9 AND is_correct = true) TO STDOUT'`
-precision=$(echo "$true_pos / ($true_pos + $false_pos)" | bc -l)
-recall=$(echo "$true_pos / ($true_pos + $false_neg)" | bc -l)
-echo "Precision: $true_pos/$((true_pos + false_pos)) = $precision"
-echo "Recall: $true_pos/$((true_pos + false_neg)) = $recall"
--- entailment/src/pheno_pathway_extract.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import sys
-
-import extractor_util as util
-
-if __name__ == '__main__':
-  for line in sys.stdin:
-    pheno, pathway_id = line.strip().split('\t')
-    util.print_tsv_output((None, pheno, pathway_id, None))
