030742edbf36531edbdfc4314825f5bfaffc1ef6
Johannes Birgmeier <jbirgmei@stanford.edu>
2015-07-22 10:42:03 -0700

    Fixes #51

--- labeling/templates/genepheno-precision/input.sql
+++ labeling/templates/genepheno-precision/input.sql
@@ -1,54 +1,80 @@
-COPY (
-  SELECT 
-    r.relation_id  as relation_id
-    , r.type as relation_type
-    , r.gene_entity as gene_name
-    , r.pheno_entity as pheno_name
-    , r.gene_wordidxs as gene_wordidxs
-    , r.pheno_wordidxs as pheno_wordidxs 
-    , string_to_array(si.words, '|^|') as words
-    --, psi.array_accum as sentences_before
-    --, fsi.array_accum as sentences_after
-    , r.expectation as expectation
-    , gm.expectation as gene_expectation
-    , pm.expectation as pheno_expectation
-    , f.features as features
-    , f.weights as weights
-  FROM
-    (SELECT *, 'association' from genepheno_association_is_correct_inference
-     OUTER JOIN
-     SELECT *, 'causation' from genepheno_causation_is_correct_inference) r
-    , sentences_input si
-    --, preceding_sentences psi
-    --, following_sentences fsi
-    , gene_mentions_is_correct_inference gm
-    , pheno_mentions_is_correct_inference pm
-    , ( -- find features relevant to the relation
-        SELECT
-          relation_id
-	  , ARRAY_AGG(feature ORDER BY abs(weight) DESC) AS features
-	  , ARRAY_AGG(weight ORDER BY abs(weight) DESC) AS weights
-	FROM 
-          genepheno_features f
-	  , dd_inference_result_weights_mapping wm
-	WHERE 
-          wm.description = ('genepheno_association_inference-' || f.feature)
-	GROUP BY 
-          relation_id
-      ) f
-  WHERE
-    r.doc_id = si.doc_id
-    AND r.sent_id = si.sent_id
-    AND r.doc_id = gm.doc_id 
-    AND r.doc_id = pm.doc_id
-    AND r.sent_id = gm.sent_id
-    AND r.sent_id = pm.sent_id
-    AND r.gene_mention_id = gm.mention_id
-    AND r.pheno_mention_id = pm.mention_id
-    AND r.relation_id = f.relation_id 
-    AND r.expectation > 0.9
-    --AND gm.expectation > 0.5
-    --AND pm.expectation > 0.5
-  ORDER BY random()
-  LIMIT 100
-) TO STDOUT WITH CSV HEADER;
+copy (
+select 
+  a.relation_id
+  , a.type as assoc_type
+  , c.type as cause_type
+  , a.gene_entity as gene_name
+  , a.pheno_entity as pheno_name
+  , a.gene_wordidxs as gene_wordidxs
+  , a.pheno_wordidxs as pheno_wordidxs
+  , string_to_array(si.words, '|^|') as words
+  , a.expectation as assoc_expectation
+  , c.expectation as cause_expectation
+  , gm.expectation as gene_expectation
+  , pm.expectation as pheno_expectation
+  , f_assoc.features as assoc_features
+  , f_assoc.weights as assoc_weights
+  , f_cause.features as cause_features
+  , f_cause.weights as cause_weights
+from
+  (select *, 'association' as is_association from genepheno_association_is_correct_inference) a
+    join
+  (select *, 'causation' as is_causation from genepheno_causation_is_correct_inference) c
+    on 
+      a.relation_id = c.relation_id
+      and a.doc_id = c.doc_id
+      and a.sent_id = c.sent_id
+      and a.gene_entity = c.gene_entity
+      and a.pheno_entity = c.pheno_entity
+      and a.gene_wordidxs = c.gene_wordidxs
+      and a.pheno_wordidxs = c.pheno_wordidxs
+  join
+    gene_mentions_is_correct_inference gm
+      on 
+        a.doc_id = gm.doc_id
+        and a.sent_id = gm.sent_id
+  join
+    pheno_mentions_is_correct_inference pm
+      on 
+        a.doc_id = pm.doc_id
+        and a.sent_id = pm.sent_id
+  join
+    sentences_input si
+      on
+        si.doc_id = a.doc_id
+        and si.sent_id = a.sent_id
+  join
+    (SELECT
+      relation_id
+      , ARRAY_AGG(feature ORDER BY abs(weight) DESC) AS features
+      , ARRAY_AGG(weight ORDER BY abs(weight) DESC) AS weights
+    FROM 
+      genepheno_features f
+      , dd_inference_result_weights_mapping wm
+    WHERE 
+      wm.description = ('genepheno_association_inference-' || f.feature)
+    GROUP BY  
+      relation_id
+    ) f_assoc
+      on
+        f_assoc.relation_id = a.relation_id
+  join
+    (SELECT
+      relation_id
+      , ARRAY_AGG(feature ORDER BY abs(weight) DESC) AS features
+      , ARRAY_AGG(weight ORDER BY abs(weight) DESC) AS weights
+    FROM 
+      genepheno_features f
+      , dd_inference_result_weights_mapping wm
+    WHERE 
+      wm.description = ('genepheno_causation_inference-' || f.feature)
+    GROUP BY  
+      relation_id
+    ) f_cause
+      on
+        f_cause.relation_id = c.relation_id
+where
+  a.expectation > 0.9 OR c.expectation > 0.9
+order by random()
+limit 100)
+to stdout with csv header;
