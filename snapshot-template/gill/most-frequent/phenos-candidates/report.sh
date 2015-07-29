aefef02707245404c5b0c825d5f9d8a000db9f38
Johannes Birgmeier <jbirgmei@stanford.edu>
2015-07-29 15:38:04 -0700

    most-frequent cleanup #71

--- snapshot-template/gill/most-frequent/phenos-candidates/report.sh
+++ snapshot-template/gill/most-frequent/phenos-candidates/report.sh
@@ -5,7 +5,7 @@ inputName=data
 outputName=data
 EXPAND_PARAMETERS=true \
 compile-xdocs "$inputName".sql.in
-run-sql "$(cat "$inputName".sql)" CSV HEADER >"$outputName".csv
+run-sql "$(cat "$inputName".sql)" CSV HEADER > "$outputName".csv
 head -n 1 "$outputName".csv > "$outputName"2.csv
 join -t $'\t' -1 1 -2 1 \
   <(tail -n+2 ${outputName}.csv | 
@@ -14,5 +14,5 @@ join -t $'\t' -1 1 -2 1 \
   <(cat ${APP_HOME}/onto/data/hpo_phenotypes.tsv | 
     cut -d '	' -f 1,2 | 
     sort -k1,1 | uniq) | 
-  awk -F '\t' '{print $1","$3","$2}' | sort -t ',' -k3,3nr >> "$outputName"2.csv
+  awk -F '\t' '{print $1","$3","$2}' | sort -t ',' -k3,3nr | sed 's/{.*}//g' >> "$outputName"2.csv
 json-for "$outputName"2.csv | transpose-json >"$outputName".json
