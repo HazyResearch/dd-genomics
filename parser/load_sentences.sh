31d4df03b3658c8c4c2c9da6846ca642734ab8b9
Johannes Birgmeier <jbirgmei@stanford.edu>
2015-08-24 17:43:56 -0700

    Changed load_sentences

--- parser/load_sentences.sh
+++ parser/load_sentences.sh
@@ -2,14 +2,13 @@
 
 source ../env_local.sh
 
-if [ "$#" -lt 3 ]; then
-  echo "Usage: $0 [INPUT: input file] [table_name] [OP: new|add]"
+if [ "$#" != 2 ]; then
+  echo "Usage: $0 [INPUT: input file] [table_name]"
   exit
 fi
 
 INPUT=$1
 TABLE=$2
-OP=$3
 
 echo "Copying to $TABLE table in database $DBNAME"
 cat $INPUT | cut -f-4,6-9,11- | psql -U $DBUSER -h $DBHOST -p $DBPORT $DBNAME -X --set ON_ERROR_STOP=1 -c "COPY $TABLE FROM STDIN LOG ERRORS INTO sentences_err SEGMENT REJECT LIMIT 100000 ROWS"
