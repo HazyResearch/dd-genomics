ba741f4671cea01eef15494b5bb582e01791536a
Johannes Birgmeier <jbirgmei@stanford.edu>
2015-08-25 11:42:28 -0700

    Most frequent inferred + a few other things #144

---
+++ snapshot-template/gill/most-frequent/pairs-causation-inf/report.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -eu
+inputName=data
+outputName=data
+EXPAND_PARAMETERS=true \
+compile-xdocs "$inputName".sql.in
+run-sql "$(cat "$inputName".sql)" CSV HEADER >"$outputName".csv
+head -n 1 "$outputName".csv > "$outputName"2.csv
+join -t $'\t' -1 2 -2 1 \
+  <(tail -n+2 ${outputName}.csv | 
+    sed 's/,/\t/g' | 
+    sort -k2,2 | uniq) \
+  <(cat ${APP_HOME}/onto/data/hpo_phenotypes.tsv | 
+    cut -d '	' -f 1,2 | 
+    sort -k1,1 | uniq) | 
+  awk -F '\t' '{print $1","$4","$2","$3}' | sort -t ',' -k4,4nr >> "$outputName"2.csv
+json-for "$outputName"2.csv | transpose-json >"$outputName".json
