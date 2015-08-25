189438cf8e70dbd0ee5b8d5e156fbc434ead85b7
Johannes Birgmeier <jbirgmei@stanford.edu>
2015-08-24 18:35:38 -0700

    load_sentences fixes #146

--- parser/load_sentences.sh
+++ parser/load_sentences.sh
@@ -11,6 +11,6 @@ INPUT=$1
 TABLE=$2
 
 echo "Copying to $TABLE table in database $DBNAME"
-cat $INPUT | cut -f-4,6-9,11- | psql -U $DBUSER -h $DBHOST -p $DBPORT $DBNAME -X --set ON_ERROR_STOP=1 -c "COPY $TABLE FROM STDIN LOG ERRORS INTO sentences_err SEGMENT REJECT LIMIT 100000 ROWS"
+cat $INPUT | cut -f1,2,4,3,6-9,11- | psql -U $DBUSER -h $DBHOST -p $DBPORT $DBNAME -X --set ON_ERROR_STOP=1 -c "COPY $TABLE FROM STDIN LOG ERRORS INTO sentences_err SEGMENT REJECT LIMIT 100000 ROWS"
 
 echo "Done."
