c4aeed35a10883089ef053d5c44749097376645a
Johannes Birgmeier <jbirgmei@stanford.edu>
2015-09-11 13:50:31 -0700

    Really fixes #182

--- util/serialize_genepheno_pairs_split.sh
+++ util/serialize_genepheno_pairs_split.sh
@@ -83,3 +83,35 @@ EOF
   psql -q -X --set ON_ERROR_STOP=1 -d $DB -f ${SQL_COMMAND_FILE} || exit 1
   rm -rf ${TMPDIR}
 done
+
+TMPDIR=$(mktemp -d /tmp/dft.XXXXXX)
+SQL_COMMAND_FILE=${TMPDIR}/dft.sql
+echo "Serializing genepheno sentences where year is null" > /dev/stderr
+cat <<EOF >> ${SQL_COMMAND_FILE}
+INSERT INTO genepheno_pairs_sentences (
+  SELECT
+    gp.doc_id, 
+    gp.section_id, 
+    gp.sent_id,
+    array_to_string(ARRAY_AGG(gp.gene_mention_id), '|^|') AS gene_mention_ids,
+    ARRAY_AGG(gp.gene_name) AS gene_names, 
+    array_to_string(ARRAY_AGG(gp.gene_wordidxs), '|^|') AS gene_wordidxs, 
+    ARRAY_AGG(gp.gene_is_correct) AS gene_is_corrects, 
+    array_to_string(ARRAY_AGG(gp.pheno_mention_id), '|^|') AS pheno_mention_ids,
+    ARRAY_AGG(gp.pheno_entity) AS pheno_entities,
+    array_to_string(ARRAY_AGG(gp.pheno_wordidxs), '|^|') AS pheno_wordidxs, 
+    ARRAY_AGG(gp.pheno_is_correct) AS pheno_is_corrects
+  FROM
+    genepheno_pairs gp 
+    LEFT OUTER JOIN doc_metadata dm 
+      ON (gp.doc_id = dm.doc_id) 
+  WHERE
+      dm.source_year is null
+  GROUP BY
+    gp.doc_id, 
+    gp.section_id, 
+    gp.sent_id
+);
+EOF
+psql -q -X --set ON_ERROR_STOP=1 -d $DB -f ${SQL_COMMAND_FILE} || exit 1
+rm -rf ${TMPDIR}
