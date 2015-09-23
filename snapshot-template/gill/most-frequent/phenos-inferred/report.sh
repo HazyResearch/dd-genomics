afb44b1b3b689a7cec06b71dcbc84017e4a7e0dd
Johannes Birgmeier <jbirgmei@stanford.edu>
2015-09-23 14:37:21 -0700

    Sorting and rev #165

--- snapshot-template/gill/most-frequent/phenos-inferred/report.sh
+++ snapshot-template/gill/most-frequent/phenos-inferred/report.sh
@@ -1,19 +1,11 @@
 #!/usr/bin/env bash
-
+# Common report.sh for data.sql.in-based report templates
+# Author: Jaeho Shin <netj@cs.stanford.edu>
+# Created: 2015-04-29
 set -eu
 inputName=data
 outputName=data
 EXPAND_PARAMETERS=true \
 compile-xdocs "$inputName".sql.in
-run-sql "$(cat "$inputName".sql)" CSV HEADER > "$outputName".csv
-head -n 1 "$outputName".csv > "$outputName"2.csv
-join -t $'\t' -1 1 -2 1 \
-  <(tail -n+2 ${outputName}.csv | 
-    sed 's/,/\t/g' | 
-    sort -k1,1 | uniq) \
-  <(cat ${APP_HOME}/onto/data/hpo_phenotypes.tsv | 
-    cut -d '	' -f 1,2 | 
-    tr ',' '/' |
-    sort -k1,1 | uniq) | 
-  awk -F '\t' '{print $1","$3","$2}' | sort -t ',' -k3,3nr | sed 's/{.*}//g' >> "$outputName"2.csv
-json-for "$outputName"2.csv | transpose-json >"$outputName".json
+run-sql "$(cat "$inputName".sql)" CSV HEADER >"$outputName".csv
+json-for "$outputName".csv | transpose-json >"$outputName".json
