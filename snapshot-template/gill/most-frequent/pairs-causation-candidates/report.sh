a9bebd4d3119a54e04a00170b092c487f42f27c0
Johannes Birgmeier <jbirgmei@stanford.edu>
2015-09-23 12:31:24 -0700

    More fixes for #165

--- snapshot-template/gill/most-frequent/pairs-causation-candidates/report.sh
+++ snapshot-template/gill/most-frequent/pairs-causation-candidates/report.sh
@@ -13,6 +13,7 @@ join -t $'\t' -1 2 -2 1 \
     sort -k2,2 | uniq) \
   <(cat ${APP_HOME}/onto/data/hpo_phenotypes.tsv | 
     cut -d '	' -f 1,2 | 
+    tr ',' '/' |
     sort -k1,1 | uniq) | 
   awk -F '\t' '{print $1","$4","$2","$3}' | sort -t ',' -k4,4nr >> "$outputName"2.csv
 json-for "$outputName"2.csv | transpose-json >"$outputName".json
--- snapshot-template/gill/most-frequent/phenos-in-cause-pair-inf/report.sh
+++ snapshot-template/gill/most-frequent/phenos-in-cause-pair-inf/report.sh
@@ -13,6 +13,7 @@ join -t $'\t' -1 1 -2 1 \
     sort -k1,1 | uniq) \
   <(cat ${APP_HOME}/onto/data/hpo_phenotypes.tsv | 
     cut -d '	' -f 1,2 | 
+    tr ',' '/' |
     sort -k1,1 | uniq) | 
   awk -F '\t' '{print $1","$3","$2}' | sort -t ',' -k3,3nr | sed 's/{.*}//g' >> "$outputName"2.csv
 json-for "$outputName"2.csv | transpose-json >"$outputName".json
