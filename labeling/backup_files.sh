8e74b8d42f70084d8620c9c84b510dcb1b881749
Johannes Birgmeier <jbirgmei@stanford.edu>
2015-09-30 16:58:41 -0700

    Added tags backup script

---
+++ labeling/backup_files.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+if [ $# -ne 1 ]
+then
+  echo "usage: $0 backup_files.txt" >> /dev/stderr
+  exit 1
+fi
+
+backup_files=$1
+
+backup_dir=`head -n 1 $backup_files`
+while true
+do
+  echo "backing up"
+  newdir=${backup_dir}/`date +'%Y-%m-%d-%H-%M-%S'`
+  mkdir -p $newdir
+  echo $newdir
+  for instr in `tail -n+2 $backup_files`
+  do
+    f=`echo $instr | cut -d ';' -f 1`
+    name=`echo $instr | cut -d ';' -f 2`
+    echo "$f $name"
+    cp $f $newdir/$name
+  done
+  sleep 60
+done
