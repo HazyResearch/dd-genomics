e81f017c24cad7b880fecf63f116b6d263b288a7
Johannes Birgmeier <jbirgmei@stanford.edu>
2015-10-13 16:38:19 -0700

    Added ensgene table

---
+++ util/load_hg19_ensGene.sh
@@ -0,0 +1,12 @@
+#! /bin/bash -e
+set -beEu -o pipefail
+
+if [ $# -ne 2 ]; then
+	echo "$0: ERROR: wrong number of arguments" >&2
+	echo "$0: USAGE: $0 DB onto/raw/hg19_ensGenes.sql" >&2
+	exit 1
+fi
+
+DB=$1
+
+psql -q -X --set ON_ERROR_STOP=1 -d $DB -f $2 || exit 1
