d61fb74b202ff5331cd355267cb0014f01d5cd2a
ThomasPalomares <thopalomares@gmail.com>
2015-10-14 08:10:23 -0700

    make it work from a tsv file from raiders

--- parser/load_sentences.sh
+++ parser/load_sentences.sh
@@ -13,6 +13,9 @@ TABLE=$2
 echo "Copying to $TABLE table in database $DBNAME"
 echo "WARNING: After loading, pay attention if ref_doc_id and sent_id in sentences is flipped!"
 echo "         This apparently happens sporadically. If it's the case, flip the dollar4 and dollar3 in the load_sentences script"
-cat $INPUT | awk -F '\t' '{OFS="\t"; print $1, $2, $4, $3, $6, $7, $8, $9, $11, $12}' | psql -U $DBUSER -h $DBHOST -p $DBPORT $DBNAME -X --set ON_ERROR_STOP=1 -c "COPY $TABLE FROM STDIN LOG ERRORS INTO sentences_err SEGMENT REJECT LIMIT 100000 ROWS"
+# cat $INPUT | awk -F '\t' '{OFS="\t"; print $1, $2, $4, $3, $6, $7, $8, $9, $11, $12}' | psql -U $DBUSER -h $DBHOST -p $DBPORT $DBNAME -X --set ON_ERROR_STOP=1 -c "COPY $TABLE FROM STDIN LOG ERRORS INTO sentences_err SEGMENT REJECT LIMIT 100000 ROWS"
+
+cat $INPUT | awk -F '\t' '{OFS="\t"; print $1, $2, $4, '\N', $5, $6, $7, $8, $9, $10}' | psql -U $DBUSER -h $DBHOST -p $DBPORT $DBNAME -X --set ON_ERROR_STOP=1 -c "COPY $TABLE FROM STDIN"
+
 
 echo "Done."
