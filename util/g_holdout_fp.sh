5534f17b4c2efbd29b4c0b5243f3b99921b933d3
Johannes Birgmeier <jbirgmei@stanford.edu>
2015-10-21 19:47:27 -0700

    Fixed the association and causation recall scripts etc

--- util/g_holdout_fp.sh
+++ util/g_holdout_fp.sh
@@ -13,11 +13,22 @@ TMPDIR=$(mktemp -d /tmp/dft.XXXXXX)
 SQL_COMMAND_FILE=${TMPDIR}/dft.sql
 cat <<EOF >> ${SQL_COMMAND_FILE}
 SELECT
-  fp.labeler LABELER_ASSOCIATION,
-  tp.tp true_pos,
-  fp.fp false_pos,
-  (tp.tp + fp.fp) positives,
-  (tp.tp::float / (tp.tp::float + fp.fp::float)) as precision
+  labeler,
+  true_pos,
+  false_pos,
+  false_neg,
+  true_neg,
+  (true_pos + false_pos) positives,
+  (true_neg + false_neg) negatives,
+  (true_pos::float / (true_pos::float + false_pos::float)) as precision,
+  (true_pos::float / (true_pos::float + false_neg::float)) as recall
+FROM (
+SELECT
+  fp.labeler labeler,
+  CASE WHEN tp.tp IS NULL THEN 0 ELSE tp.tp END true_pos,
+  CASE WHEN fp.fp IS NULL THEN 0 ELSE fp.fp END false_pos,
+  CASE WHEN fn.fn IS NULL THEN 0 ELSE fn.fn END false_neg,
+  CASE WHEN tn.tn IS NULL THEN 0 ELSE tn.tn END true_neg
 FROM
   (SELECT
     labeler,
@@ -32,7 +43,7 @@ FROM
     g.expectation > 0.9 
     AND l.is_correct = 'f'
   GROUP BY labeler) fp
-  JOIN
+  FULL OUTER JOIN
   (SELECT
     labeler,
     COUNT(DISTINCT mention_id) tp
@@ -46,7 +57,37 @@ FROM
     g.expectation > 0.9 
     AND l.is_correct = 't'
   GROUP BY labeler) tp
-  ON (fp.labeler = tp.labeler);
+  ON (fp.labeler = tp.labeler)
+  FULL OUTER JOIN
+  (SELECT
+    labeler,
+    COUNT(DISTINCT mention_id) fn
+  FROM
+    gene_mentions_filtered_is_correct_inference g 
+    JOIN gene_holdout_set s 
+      ON (s.doc_id = g.doc_id AND s.section_id = g.section_id AND s.sent_id = g.sent_id AND (STRING_TO_ARRAY(g.wordidxs, '|^|'))::int[] = s.gene_wordidxs) 
+    JOIN gene_holdout_labels l
+      ON (s.doc_id = l.doc_id AND s.section_id = l.section_id AND s.sent_id = l.sent_id) 
+  WHERe
+    g.expectation <= 0.9 
+    AND l.is_correct = 't'
+  GROUP BY labeler) fn
+  ON (fp.labeler = fn.labeler)
+  FULL OUTER JOIN
+  (SELECT
+    labeler,
+    COUNT(DISTINCT mention_id) tn
+  FROM
+    gene_mentions_filtered_is_correct_inference g 
+    JOIN gene_holdout_set s 
+      ON (s.doc_id = g.doc_id AND s.section_id = g.section_id AND s.sent_id = g.sent_id AND (STRING_TO_ARRAY(g.wordidxs, '|^|'))::int[] = s.gene_wordidxs) 
+    JOIN gene_holdout_labels l
+      ON (s.doc_id = l.doc_id AND s.section_id = l.section_id AND s.sent_id = l.sent_id) 
+  WHERe
+    g.expectation <= 0.9 
+    AND l.is_correct = 'f'
+  GROUP BY labeler) tn
+  ON (fp.labeler = tn.labeler)) a;
 EOF
 psql -q -X --set ON_ERROR_STOP=1 -d $DB -f ${SQL_COMMAND_FILE} > /dev/stderr
 rm -rf ${TMPDIR}
--- util/gp_holdout_fp_caus.sh
+++ util/gp_holdout_fp_caus.sh
@@ -13,11 +13,20 @@ TMPDIR=$(mktemp -d /tmp/dft.XXXXXX)
 SQL_COMMAND_FILE=${TMPDIR}/dft.sql
 cat <<EOF >> ${SQL_COMMAND_FILE}
 SELECT
-  fp.labeler LABELER_CAUSATION,
-  tp.tp true_pos,
-  fp.fp false_pos,
-  (tp.tp + fp.fp) positives,
-  (tp.tp::float / (tp.tp::float + fp.fp::float)) as precision
+  labeler,
+  true_pos,
+  true_neg,
+  false_pos,
+  false_neg,
+  (true_pos::float / CASE WHEN (true_pos::float + false_pos::float) <> 0 THEN (true_pos::float + false_pos::float) ELSE 1 END) as precision,
+  (true_pos::float / CASE WHEN (true_pos::float + false_neg::float) <> 0 THEN (true_pos::float + false_neg::float) ELSE 1 END) as recall
+FROM (
+SELECT
+  COALESCE(fp.labeler, tp.labeler, fn.labeler, tn.labeler) labeler,
+  CASE WHEN tp.tp IS NULL THEN 0 ELSE tp.tp END true_pos,
+  CASE WHEN fp.fp IS NULL THEN 0 ELSE fp.fp END false_pos,
+  CASE WHEN fn.fn IS NULL THEN 0 ELSE fn.fn END false_neg,
+  CASE WHEN tn.tn IS NULL THEN 0 ELSE tn.tn END true_neg
 FROM
   (SELECT
     labeler,
@@ -32,7 +41,7 @@ FROM
     gc.expectation > 0.9 
     AND l.is_correct = 'f'
   GROUP BY labeler) fp
-  JOIN
+  FULL OUTER JOIN
   (SELECT
     labeler,
     COUNT(DISTINCT relation_id) tp
@@ -46,7 +55,37 @@ FROM
     gc.expectation > 0.9 
     AND l.is_correct = 't'
   GROUP BY labeler) tp
-  ON (fp.labeler = tp.labeler);
+  ON (fp.labeler = tp.labeler)
+  FULL OUTER JOIN
+  (SELECT
+    labeler,
+    COUNT(DISTINCT relation_id) fn
+  FROM
+    genepheno_causation_is_correct_inference gc 
+    JOIN genepheno_holdout_set s 
+      ON (s.doc_id = gc.doc_id AND s.section_id = gc.section_id AND s.sent_id = gc.sent_id AND gc.gene_wordidxs = s.gene_wordidxs AND gc.pheno_wordidxs = s.pheno_wordidxs) 
+    JOIN genepheno_holdout_labels l
+      ON (s.doc_id = l.doc_id AND s.section_id = l.section_id AND s.sent_id = l.sent_id) 
+  WHERe
+    gc.expectation <= 0.9
+    AND l.is_correct = 't'
+  GROUP BY labeler) fn
+  ON (tp.labeler = fn.labeler)
+  FULL OUTER JOIN
+  (SELECT
+    labeler,
+    COUNT(DISTINCT relation_id) tn
+  FROM
+    genepheno_causation_is_correct_inference gc 
+    JOIN genepheno_holdout_set s 
+      ON (s.doc_id = gc.doc_id AND s.section_id = gc.section_id AND s.sent_id = gc.sent_id AND gc.gene_wordidxs = s.gene_wordidxs AND gc.pheno_wordidxs = s.pheno_wordidxs) 
+    JOIN genepheno_holdout_labels l
+      ON (s.doc_id = l.doc_id AND s.section_id = l.section_id AND s.sent_id = l.sent_id) 
+  WHERe
+    gc.expectation <= 0.9
+    AND l.is_correct = 'f'
+  GROUP BY labeler) tn
+  ON (fn.labeler = tn.labeler)) a;
 EOF
 psql -q -X --set ON_ERROR_STOP=1 -d $DB -f ${SQL_COMMAND_FILE} > /dev/stderr
 rm -rf ${TMPDIR}
