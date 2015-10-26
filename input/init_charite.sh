a91bb05099497ab9b29b33951abfda293644dbc9
ThomasPalomares <thopalomares@gmail.com>
2015-10-26 14:20:15 -0700

    progress ddlog

--- input/init_charite.sh
+++ input/init_charite.sh
@@ -4,4 +4,4 @@ ${APP_HOME}/util/truncate_table.sh ${DBNAME} charite
 
 cat ${APP_HOME}/onto/manual/harendra_phenotype_to_gene.map | deepdive sql """COPY charite FROM STDIN DELIMITER '	' """
 
-${APP_HOME}/util/uniq_table.sh ${DBNAME} charite_canon
+${APP_HOME}/util/uniq_table.sh ${DBNAME} charite
