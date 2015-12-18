270f3952925c092c276826da0aa988735815f7d7
Johannes Birgmeier <jbirgmei@stanford.edu>
2015-12-18 13:00:08 -0800

    Printing match paths again

--- code/genepheno_sv_new.py
+++ code/genepheno_sv_new.py
@@ -126,57 +126,62 @@ if __name__ == '__main__':
   # hard_filters = config.GENE_PHENO_ASSOCIATION['HF']
   app_home = os.environ['APP_HOME']
   match_trees = []
-  with open(app_home + '/true_causation_sentences1.tsv') as f:
-    for line in f:
-      row = ds_parser.parse_tsv_row(line)
+  with open(app_home + '/match_paths/match_paths%d.txt' % random.randint(0, 100000), 'a') as match_path_file:
+    with open(app_home + '/true_causation_sentences1.tsv') as f:
+      for line in f:
+        row = ds_parser.parse_tsv_row(line)
+        try:
+          match_trees.append(row_to_canonical_match_tree(row, [row.gene_wordidxs, row.pheno_wordidxs]))
+        except (DepParentsCycleException, OverlappingCandidatesException, RootException):
+          continue
+    mt_root1, match_tree1 = match_trees[0]
+    for mt_root0, match_tree0 in match_trees[1:]:
+      # for i, mc in enumerate(match_tree1):
+      #   print >>sys.stderr, str(i+1) + ": " + str(mc)
+      mda = MultiDepAlignment(mt_root0, match_tree0, mt_root1, match_tree1, 2, \
+                                  [set(['disease', 'disorder']), \
+                                   set(['mutation', 'variant', 'allele', 'polymorphism']), \
+                                   set(['case', 'patient']), \
+                                   set(['identify', 'report', 'find', 'detect']), \
+                                   set(['cause', 'associate', 'link', 'lead']),
+                                   set(['mutation', 'inhibition'])])
+      mt_root1, match_tree1 = mda.get_match_tree()
+      
+    # mt_root1, match_tree1 = match_trees[0]
+    mda.print_match_path(match_path_file)
+    lc = 0
+    start_time = time.time()
+    for line in sys.stdin:
+      lc += 1
+      row = parser.parse_tsv_row(line)
+      if row.gene_is_correct == False or row.pheno_is_correct == False:
+        continue
       try:
-        match_trees.append(row_to_canonical_match_tree(row, [row.gene_wordidxs, row.pheno_wordidxs]))
+        mt_root2, match_tree2 = row_to_canonical_match_tree(row, [row.gene_wordidxs, row.pheno_wordidxs])
+        assert len(match_tree2) <= len(row.words) + 1, (len(row.words), len(match_tree2), row.words, match_tree2) 
       except (DepParentsCycleException, OverlappingCandidatesException, RootException):
         continue
-  mt_root1, match_tree1 = match_trees[0]
-  for mt_root0, match_tree0 in match_trees[1:]:
-    # for i, mc in enumerate(match_tree1):
-    #   print >>sys.stderr, str(i+1) + ": " + str(mc)
-    mda = MultiDepAlignment(mt_root0, match_tree0, mt_root1, match_tree1, 2, \
-                                [set(['disease', 'disorder']), \
-                                 set(['mutation', 'variant', 'allele', 'polymorphism']), \
-                                 set(['case', 'patient']), \
-                                 set(['identify', 'report', 'find', 'detect']), \
-                                 set(['cause', 'associate', 'link', 'lead'])])
-    mt_root1, match_tree1 = mda.get_match_tree()
-    
-  # mt_root1, match_tree1 = match_trees[0]
-  lc = 0
-  start_time = time.time()
-  # with open(app_home + '/match_paths%d.txt' % random.randint(0, 100000), 'a') as f:
-  for line in sys.stdin:
-    lc += 1
-    row = parser.parse_tsv_row(line)
-    if row.gene_is_correct == False or row.pheno_is_correct == False:
-      continue
-    try:
-      mt_root2, match_tree2 = row_to_canonical_match_tree(row, [row.gene_wordidxs, row.pheno_wordidxs])
-      assert len(match_tree2) <= len(row.words) + 1, (len(row.words), len(match_tree2), row.words, match_tree2) 
-    except (DepParentsCycleException, OverlappingCandidatesException, RootException):
-      continue
-    matching_scores = []
-    rescores = []
-    # for (mt_root1, match_tree1) in match_trees:
-    mda = MultiDepAlignment(mt_root1, match_tree1, mt_root2, match_tree2, 2, \
-                            [set(['disease', 'disorder']), \
-                             set(['mutation', 'variant', 'allele', 'polymorphism']), \
-                             set(['case', 'patient']), \
-                             set(['identify', 'report', 'find', 'detect']), \
-                             set(['cause', 'associate', 'link', 'lead'])])
-    # mda.print_matched_lemmas(f)
-    # mda.print_match_path(f)
-    score1 = mda.overall_score()
-    score2 = mda.rescore([(set(['cause', 'lead']), set(['associate', 'link']), -20)])
-    r = read_candidate(row)
-    matching_scores.append(int(score1))
-    rescores.append(int(score1 + score2))
-    # end for
-    eutil.print_tsv_output(r._replace(matching_scores=matching_scores, rescores=rescores))
-  end_time = time.time()
-  if lc != 0:
-    print >>sys.stderr, "Number of lines: %d, Time per line: %f seconds" % (lc, (end_time - start_time) / (float(lc)))
+      matching_scores = []
+      rescores = []
+      # for (mt_root1, match_tree1) in match_trees:
+      mda = MultiDepAlignment(mt_root1, match_tree1, mt_root2, match_tree2, 2, \
+                              [set(['disease', 'disorder']), \
+                               set(['mutation', 'variant', 'allele', 'polymorphism']), \
+                               set(['case', 'patient']), \
+                               set(['identify', 'report', 'find', 'detect']), \
+                               set(['cause', 'associate', 'link', 'lead']),
+                               set(['mutation', 'inhibition'])])
+      # mda.print_matched_lemmas(match_path_file)
+      print >>match_path_file, ' '.join(row.words)
+      mda.print_match_path(match_path_file)
+      score1 = mda.overall_score()
+      score2 = mda.rescore([(set(['cause', 'lead']), set(['associate', 'link']), -50), 
+                            (set(['mutation']), set(['inhibition']), -50)])
+      r = read_candidate(row)
+      matching_scores.append(int(score1))
+      rescores.append(int(score1 + score2))
+      # end for
+      eutil.print_tsv_output(r._replace(matching_scores=matching_scores, rescores=rescores))
+    end_time = time.time()
+    if lc != 0:
+      print >>sys.stderr, "Number of lines: %d, Time per line: %f seconds" % (lc, (end_time - start_time) / (float(lc)))
