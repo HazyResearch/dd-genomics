b70654c8ea739917382f4a691801c9a4b1bfd02d
Alexander Ratner <ajratner@gmail.com>
2016-01-13 20:16:18 -0800

    Fixed error with set size changing during iteration over it

--- code/create_allowed_phenos_list.py
+++ code/create_allowed_phenos_list.py
@@ -5,7 +5,7 @@ from data_util import get_hpo_phenos, get_parents, read_hpo_dag
 if __name__ == "__main__":
   hpo_dag = read_hpo_dag()
   allowed_phenos = set(get_hpo_phenos(hpo_dag))
-  for hpo_id in allowed_phenos:
+  for hpo_id in allowed_phenos.copy():
     parent_ids = get_parents(hpo_id, hpo_dag) # includes the original hpo_id
     assert hpo_id in parent_ids
     if 'HP:0000118' not in parent_ids:
