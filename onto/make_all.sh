b226e47d266e97b45e4db027afbc0261229ddd97
Johannes Birgmeier <jbirgmei@stanford.edu>
2016-01-14 10:54:51 -0800

    Added UCSC ensembl protein table

--- onto/make_all.sh
+++ onto/make_all.sh
@@ -130,7 +130,7 @@ fi
 
 if [ ! -f data/ucscEnsemblProteinTable.tsv ]; then
   if [ -f /dfs/scratch0/jbirgmei/dd_onto_data/ucscEnsemblProteinTable.tsv ]; then
-    cp /dfs/scratch0/jbirgmei/dd_onto_data/ucscEnsemblProteinTable.tsv raw/ucscEnsemblProteinTable.tsv
+    cp /dfs/scratch0/jbirgmei/dd_onto_data/ucscEnsemblProteinTable.tsv data/ucscEnsemblProteinTable.tsv
   else
     echo " Copying ucscEnsemblProteinTable.tsv from local filesystem failed."
     echo " If have access to raiders2, run scp username@raiders2:/dfs/scratch0/jbirgmei/dd_onto_data/ucscEnsemblProteinTable.tsv data/ucscEnsemblProteinTable.tsv"
