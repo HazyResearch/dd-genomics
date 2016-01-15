f389722a49b151ac6ea2d49b5c98d9c447a5245c
ThomasPalomares <thopalomares@gmail.com>
2016-01-15 13:05:25 -0800

    keeping it for easy merge with master

---
+++ run.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+export JAVA_TOOL_OPTIONS="-Dfile.encoding=UTF-8" 
+
+if [ -f env_local.sh ]; then
+  echo "Using env_local.sh"
+  source ./env_local.sh
+else
+  echo "Using env.sh"
+  source ./env.sh
+fi
+
+
+if [ -f $DEEPDIVE_HOME/sbt/sbt ]; then
+  echo "DeepDive $DEEPDIVE_HOME"
+else
+  echo "[ERROR] Could not find sbt in $DEEPDIVE_HOME!"
+  exit 1
+fi
+
+unset GDD_PIPELINE
+if [ "${1-}" == "" ]; then
+  echo "Usage: ./run.sh [PIPELINE_TO_RUN]"
+  exit 1
+else
+  export GDD_PIPELINE="$1"
+fi
+
+# Launch gpfdist if not launched.
+gpfdist -d $GPPATH -p $GPPORT &
+gpfdist_pid=$!
+trap "kill $gpfdist_pid" EXIT
+
+cd $DEEPDIVE_HOME
+sbt "run -c $APP_HOME/${APP_CONF:-application.conf}"
+
+echo "Don't forget to VACUUM VERBOSE; in psql once in a while!"
