4afa908c5aff45db4628fb0b8295ee84056debbc
Johannes Birgmeier <jbirgmei@stanford.edu>
2016-01-15 16:55:08 -0800

    Fixed pulling all gene holdout sets

--- onto/manual/get_historical_g_holdout_sets.sh
+++ onto/manual/get_historical_g_holdout_sets.sh
@@ -5,5 +5,5 @@ git log --pretty=oneline gene_holdout_set.tsv |
   xargs -L 1 -I {} git show {}:onto/manual/gene_holdout_set.tsv | 
   grep '{' |
   sed 's/|~|/,/g' | 
-  awk '{if (NF == 5) print $0}' | 
+  awk '{if (NF == 4) print $0}' | 
   sort | uniq | shuf # > all_gene_holdout_sets.tsv
--- onto/manual/get_historical_g_labels.sh
+++ onto/manual/get_historical_g_labels.sh
@@ -4,5 +4,5 @@ git log --pretty=oneline gene_holdout_labels.tsv |
   cut -d ' ' -f 1 | 
   xargs -L 1 -I {} git show {}:onto/manual/gene_holdout_labels.tsv | 
   sed 's/|~|/,/g' | 
-  awk '{if (NF == 6) print $0}' | 
+  awk '{if (NF == 5) print $0}' | 
   sort | uniq | shuf # > all_gene_holdout_labels.tsv
