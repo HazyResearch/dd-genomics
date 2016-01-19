28b9fccc2845f3605df2e86111a88751d2e1321c
Johannes Birgmeier <jbirgmei@stanford.edu>
2016-01-18 16:18:48 -0800

    More stuff in pre-holdout evaluation

--- util/gp_create_holdout_sentences.sh
+++ util/gp_create_holdout_sentences.sh
@@ -12,6 +12,20 @@ DB=$1
 TMPDIR=$(mktemp -d /tmp/dft.XXXXXX)
 SQL_COMMAND_FILE=${TMPDIR}/dft.sql
 cat <<EOF >> ${SQL_COMMAND_FILE}
+DELETE FROM genepheno_holdout_set 
+WHERE (doc_id, section_id, sent_id, gene_wordidxs, pheno_wordidxs) IN 
+(SELECT DISTINCT
+  s.* 
+FROM
+  genepheno_holdout_set s 
+  LEFT JOIN 
+  genepheno_pairs p 
+    ON (s.doc_id = p.doc_id AND s.section_id = p.section_id AND s.sent_id = p.sent_id AND s.gene_wordidxs = STRING_TO_ARRAY(p.gene_wordidxs, '|~|')::INTEGER[] AND s.pheno_wordidxs = STRING_TO_ARRAY(p.pheno_wordidxs, '|~|')::INTEGER[]) WHERE p.doc_id IS NULL);
+
+DELETE FROM genepheno_holdout_labels 
+WHERE (doc_id, section_id, sent_id) NOT IN 
+(SELECT DISTINCT doc_id, section_id, sent_id FROM genepheno_holdout_set);
+
 DROP TABLE IF EXISTS sentences_input_with_holdout_gp;
 CREATE TABLE sentences_input_with_holdout_gp AS (
   SELECT si.*
