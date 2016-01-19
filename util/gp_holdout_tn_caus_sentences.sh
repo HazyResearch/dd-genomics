816ba03127c0819f5e9ae5bd1bdaf007c91a8f1e
Johannes Birgmeier <jbirgmei@stanford.edu>
2016-01-18 16:39:51 -0800

    More fixes in eval

--- util/gp_holdout_tn_caus_sentences.sh
+++ util/gp_holdout_tn_caus_sentences.sh
@@ -29,7 +29,7 @@ FROM
   RIGHT JOIN genepheno_holdout_set s 
     ON (s.doc_id = gc.doc_id AND s.section_id = gc.section_id AND s.sent_id = gc.sent_id AND gc.gene_wordidxs = s.gene_wordidxs AND gc.pheno_wordidxs = s.pheno_wordidxs) 
   JOIN genepheno_holdout_labels l
-    ON (s.doc_id = l.doc_id AND s.section_id = l.section_id AND s.sent_id = l.sent_id and l.type = 'causation') 
+    ON (s.doc_id = l.doc_id AND s.section_id = l.section_id AND s.sent_id = l.sent_id) 
   JOIN sentences_input_with_holdout_gp si
     ON (si.doc_id = l.doc_id AND si.section_id = l.section_id AND si.sent_id = l.sent_id)
 WHERE
