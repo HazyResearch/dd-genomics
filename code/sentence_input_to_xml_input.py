7f3b46b82b575bdf27973141a1eb919af0f3c9b0
Alexander Ratner <ajratner@gmail.com>
2016-01-21 08:17:34 -0800

    Basic XML preprocessing working

--- code/sentence_input_to_xml_input.py
+++ code/sentence_input_to_xml_input.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python
 import sys
-from treedlib_util import sentence_input_to_xml_input
-from extractor_util import print_tsv_output
+from treedlib_util import corenlp_parser, print_tsv_output
+from treedlib_structs import corenlp_to_xmltree
 
-for line in sys.stdin:
-  try:
-    x = sentence_input_to_xml_input(line)
-  except:
-    pass
-  if x is not None:
-    print_tsv_output(x)
+for row in corenlp_parser.parse_stdin():
+  xt = corenlp_to_xmltree(row)
+  if xt is not None:
+    print_tsv_output((row.doc_id, row.section_id, row.sent_id, xt.to_str()))
