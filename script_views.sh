fa376b1d39da17e507a337ab8d6859ad6735d60f
ThomasPalomares <thopalomares@gmail.com>
2016-01-27 17:32:21 -0800

    fixing script without sentences_input

--- script_views.sh
+++ script_views.sh
@@ -20,18 +20,17 @@ while true; do
                                 exit;;
         * ) echo "Please answer y or n.";;
     esac
-done
-echo 
+done 
 
 while true; do
-    read -p "Is sentences_input the same as for the previous run of this script? Answer no only if you modified sentences_input or if it's the first time you run this script." yn
+    read -p "Is sentences_input the same as for the previous run of this script? Answer no only if you modified sentences_input or if it's the first time you run this script:" yn
     case $yn in
-        [Yy]* ) sentences_to_be_re_run=$false; break;;
-        [Nn]* ) sentences_to_be_re_run=$true; break;;
+        [Yy]* ) sentences_to_be_re_run="false"; break;;
+        [Nn]* ) sentences_to_be_re_run="true"; break;;
         * ) echo "Please answer y or n.";;
     esac
 done
-echo 
+
 
 if [[ ( -f db_for_gp.url)  ]]
 then
@@ -41,16 +40,16 @@ if [[ ( -f db.url)  ]]
 then
         cp db.url db_for_gp.url
 fi
+
 database_greenplum=$(cat db_for_gp.url | sed 's/.*:\/\/.*\///')
-# Thomas own database in case 5432 doesn't work:
-# echo "postgresql://localhost:15193/${database_greenplum}_for_views" > db_for_pg.url
 echo "postgresql://localhost:5432/${database_greenplum}_for_views" > db_for_pg.url
 
-deepdive redo weights
-deepdive redo dd_inference_result_variables_mapped_weights_bis
+DEEPDIVE_PLAN_EDIT=false deepdive redo weights
+DEEPDIVE_PLAN_EDIT=false deepdive redo dd_inference_result_variables_mapped_weights_bis
 deepdive sql "insert into dd_inference_result_variables_mapped_weights_bis select * from dd_inference_result_variables_mapped_weights;"
 
-if [ $sentences_to_be_re_run]
+
+if $sentences_to_be_re_run
 then   
     deepdive mark todo sentences_input_views
 else
@@ -62,51 +61,73 @@ deepdive mark todo genepheno_association_views
 deepdive mark todo genepheno_causation_views
 deepdive mark todo pheno_mentions_views
 
-deepdive redo dumb_for_views
+DEEPDIVE_PLAN_EDIT=false deepdive redo dumb_for_views
 
 database_greenplum=$(cat db_for_gp.url | sed 's/.*:\/\/.*\///')
 
 mkdir -p ../tables_for_views
 export PATH=/dfs/scratch0/netj/postgresql/9.4.4/bin:$PATH
 
-if [ $sentences_to_be_re_run]
+if $sentences_to_be_re_run
 then   
     pg_dump -p 6432 -h raiders7 -U tpalo ${database_greenplum} -t sentences_input_views > ../tables_for_views/sentences_input_views.sql
+    echo 'table sentences_input_views.sql dumped'
 fi
 
 pg_dump -p 6432 -h raiders7 -U tpalo ${database_greenplum} -t gene_mentions_views > ../tables_for_views/gene_mentions_views.sql
+echo 'table gene_mentions_views.sql dumped'
 pg_dump -p 6432 -h raiders7 -U tpalo ${database_greenplum} -t genepheno_causation_views > ../tables_for_views/genepheno_causation_views.sql
+echo 'table genepheno_causation_views.sql dumped'
 pg_dump -p 6432 -h raiders7 -U tpalo ${database_greenplum} -t pheno_mentions_views > ../tables_for_views/pheno_mentions_views.sql
+echo 'table pheno_mentions_views.sql dumped'
 pg_dump -p 6432 -h raiders7 -U tpalo ${database_greenplum} -t genepheno_association_views > ../tables_for_views/genepheno_association_views.sql
+echo 'table genepheno_association_views.sql dumped'
 pg_dump -p 6432 -h raiders7 -U tpalo ${database_greenplum} -t gene_mentions_filtered_inference_label_inference > ../tables_for_views/gene_mentions_filtered_inference_label_inference.sql
+echo 'table gene_mentions_filtered_inference_label_inference.sql dumped'
 pg_dump -p 6432 -h raiders7 -U tpalo ${database_greenplum} -t genepheno_causation_inference_label_inference > ../tables_for_views/genepheno_causation_inference_label_inference.sql
+echo 'table genepheno_causation_inference_label_inference.sql dumped'
 pg_dump -p 6432 -h raiders7 -U tpalo ${database_greenplum} -t genepheno_association_inference_label_inference > ../tables_for_views/genepheno_association_inference_label_inference.sql
+echo 'table genepheno_association_inference_label_inference.sql dumped'
 pg_dump -p 6432 -h raiders7 -U tpalo ${database_greenplum} -t dd_inference_result_variables > ../tables_for_views/dd_inference_result_variables.sql
+echo 'table dd_inference_result_variables.sql dumped'
 pg_dump -p 6432 -h raiders7 -U tpalo ${database_greenplum} -t gene_mentions_filtered_inference > ../tables_for_views/gene_mentions_filtered_inference.sql
+echo 'table gene_mentions_filtered_inference.sql dumped'
 pg_dump -p 6432 -h raiders7 -U tpalo ${database_greenplum} -t genepheno_causation_inference > ../tables_for_views/genepheno_causation_inference.sql
+echo 'table genepheno_causation_inference.sql dumped'
 pg_dump -p 6432 -h raiders7 -U tpalo ${database_greenplum} -t genepheno_association_inference > ../tables_for_views/genepheno_association_inference.sql
-
+echo 'table genepheno_association_inference.sql dumped'
 
 
 cp db_for_pg.url db.url
 
-if [ $sentences_to_be_re_run]
+if $sentences_to_be_re_run
 then   
-    deepdive redo init/db
+    DEEPDIVE_PLAN_EDIT=false deepdive redo init/db
     deepdive sql < ../tables_for_views/sentences_input_views.sql 
     deepdive sql "update sentences_input_views set words = replace(words, '|^|', ' ');"
 fi
 
+deepdive sql 'truncate table genepheno_causation_views.sql'
 deepdive sql < ../tables_for_views/genepheno_causation_views.sql
+deepdive sql 'truncate table genepheno_association_views.sql'
 deepdive sql < ../tables_for_views/genepheno_association_views.sql 
+deepdive sql 'truncate table gene_mentions_views.sql'
 deepdive sql < ../tables_for_views/gene_mentions_views.sql 
+deepdive sql 'truncate table pheno_mentions_views.sql'
 deepdive sql < ../tables_for_views/pheno_mentions_views.sql 
+deepdive sql 'truncate table dd_inference_result_variables.sql'
 deepdive sql < ../tables_for_views/dd_inference_result_variables.sql
+deepdive sql 'truncate table gene_mentions_filtered_inference.sql'
 deepdive sql < ../tables_for_views/gene_mentions_filtered_inference.sql 
+deepdive sql 'truncate table gene_mentions_filtered_inference_label_inference.sql'
 deepdive sql < ../tables_for_views/gene_mentions_filtered_inference_label_inference.sql 
+deepdive sql 'truncate table genepheno_causation_inference.sql'
 deepdive sql < ../tables_for_views/genepheno_causation_inference.sql 
+deepdive sql 'truncate table genepheno_causation_inference_label_inference.sql'
 deepdive sql < ../tables_for_views/genepheno_causation_inference_label_inference.sql 
+deepdive sql 'truncate table genepheno_association_inference.sql'
 deepdive sql < ../tables_for_views/genepheno_association_inference.sql 
+deepdive sql 'truncate table genepheno_association_inference_label_inference.sql'
 deepdive sql < ../tables_for_views/genepheno_association_inference_label_inference.sql 
 
 
@@ -117,11 +138,11 @@ rm -r ../tables_for_views
 export ELASTICSEARCH_BASEURL=http://localhost:9${RANDOM:0:3}
 
 #this bulk batchsize could be increased to increase speed. Careful, from 200000, it starts not to work so well.
-export ELASTICSEARCH_BULK_BATCHSIZE=30000
+export ELASTICSEARCH_BULK_BATCHSIZE=20000
 
-if [ $sentences_to_be_re_run]
+if $sentences_to_be_re_run
 then   
-    mindbender search drop
+   # mindbender search drop
     mindbender search update 
 else
     mindbender search update genepheno_association_views genepheno_causation_views gene_mentions_views pheno_mentions_views
@@ -129,6 +150,4 @@ fi
 
 cp db_for_gp.url db.url
 
-#export ES_HEAP_SIZE=10g
-
-#PORT=$RANDOM mindbender search gui
+echo 'Great! now you can launch the views with the command: export ES_HEAP_SIZE=10g; PORT=$RANDOM mindbender search gui'
