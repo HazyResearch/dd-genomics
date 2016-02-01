35b02f66bac0dc957bea0e5b737a0f2be3b28a63
Alexander Ratner <ajratner@gmail.com>
2016-02-01 02:10:32 -0800

    Re-writing GP DSR code

---
+++ code/genepheno_causation_supervision_old.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+
+import collections
+import extractor_util as util
+import data_util as dutil
+import dep_util as deps
+import os
+import random
+import re
+import sys
+import config
+
+import genepheno_supervision_util as sv
+
+if __name__ == '__main__':
+  sr = config.GENE_PHENO_CAUSATION['SR']
+  hf = config.GENE_PHENO_CAUSATION['HF']
+  sv.supervise(sr, hf)
