6d465ab77de20680a097061e7ef8dc5a613658c1
Youssef Ahres <youssefahres@gmail.com>
2016-02-09 13:49:18 -0800

    extending evaluation pipeline to precision #317

--- evaluation.sh
+++ evaluation.sh
@@ -2,7 +2,7 @@
 
 if [ $# -lt 2 ]; then
         echo "$0: ERROR: wrong number of arguments" >&2
-        echo "$0: USAGE: $0 {gene,causation,association} VERSION [CONFIDENCE] [OPTOUT]" >&2
+        echo "$0: USAGE: $0 {gene,causation,association, causation_precision} VERSION [CONFIDENCE] [OPTOUT]" >&2
 	echo "$0: See README for info on options"
         exit 1
 fi
@@ -25,8 +25,10 @@ elif [ $1 = 'causation' ]; then
         results_log/compute_causation_stats.sh $VERSION $CONFIDENCE $OPTOUT
 elif [ $1 = 'association' ]; then
 	results_log/compute_association_stats.sh $VERSION $CONFIDENCE $OPTOUT
+elif [ $1 = 'causation_precision' ]; then
+        results_log/compute_causation_precision_stats.sh $VERSION $CONFIDENCE $OPTOUT
 else
         echo "Argument not valid"
-        echo "$0: USAGE: $0 {gene,causation,association} VERSION [CONFIDENCE] [OPTOUT]" >&2
+        echo "$0: USAGE: $0 {gene,causation,association,causation_precision} VERSION [CONFIDENCE] [OPTOUT]" >&2
         exit 1
 fi
---
+++ results_log/compute_causation_precision_stats.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+. $GDD_HOME/env_local.sh
+
+if [ $# -ge 1 ]; then
+        echo "Setting version to $1"
+        VERSION=$1
+else
+        echo "Setting version to 0"
+        VERSION=0
+fi
+
+if [ $# -ge 2 ]; then
+        echo "Setting confidence to $2"
+        CONFIDENCE=$1
+else
+        echo "Setting confidence to 0.9"
+        CONFIDENCE=.9
+fi
+
+# extract all labels from the genepheno labels that are true
+deepdive sql "COPY (SELECT gal.relation_id, gal.is_correct, gal.labeler FROM genepheno_causation_precision_labels gal, genepheno_pairs gp WHERE (gp.gene_mention_id || ('_' || gp.pheno_mention_id)) = gal.relation_id AND gal.version = $VERSION) TO STDOUT WITH NULL AS ''" > genepheno_causation_labels.tsv
+
+# extract all predictions from the genepheno_causation table
+deepdive sql "COPY (SELECT gi.relation_id, gi.expectation FROM genepheno_causation_inference_label_inference gi, genepheno_causation_precision_labels gal WHERE gal.relation_id = gi.relation_id AND gal.version = $VERSION) TO STDOUT WITH NULL AS ''" > genepheno_causation_predictions.tsv
+# launch python script that computes precision and recall
+$GDD_HOME/results_log/compute_stats_helper.py genepheno_causation_labels.tsv genepheno_causation_predictions.tsv $CONFIDENCE > $GDD_HOME/results_log/latest_stats_causation_precision.tsv
+rm genepheno_causation_labels.tsv
+rm genepheno_causation_predictions.tsv
+
+if [ $# -eq 3 ] && [ $3 = 'OPTOUT' ]; then
+        echo"NO LOGGING"
+        exit 0
+fi
+
+# Store logs
+DIR_NAME=$GDD_HOME/results_log/$DDUSER/causation_precision-`date +'%m-%d-%Y-%H-%M-%S'`
+mkdir -p $DIR_NAME
+
+# Store stats in log
+cat $GDD_HOME/results_log/latest_stats_causation_precision.tsv > $DIR_NAME/stats_causation_precision.tsv
+
+# Store TP/FP/TN/FN
+# TP is trivial, we just need to see where we have true label with confidence > $CONFIDENCE
+deepdive sql "COPY (SELECT gal.relation_id, gal.is_correct, gal.labeler, gi.expectation  FROM genepheno_causation_labels gal, genepheno_causation_inference_label_inference gi WHERE gal.relation_id = gi.relation_id AND gi.expectation >= $CONFIDENCE AND gal.is_correct = 't' AND gal.version = $VERSION) TO STDOUT WITH NULL AS ''" > $DIR_NAME/TP.tsv
+
+# FP is a bit more complex since we need to join with genepheno_pairs to avoid FP on documents that we are ejecting (cancer suff)
+deepdive sql "COPY (SELECT gal.relation_id, gal.is_correct, gal.labeler, gi.expectation  FROM genepheno_causation_labels gal, genepheno_causation_inference_label_inference gi, genepheno_pairs gp WHERE gal.relation_id = gi.relation_id AND gi.expectation >= $CONFIDENCE AND gal.is_correct = 'f' AND (gp.gene_mention_id || ('_' || gp.pheno_mention_id)) = gal.relation_id AND gal.version = $VERSION) TO STDOUT WITH NULL AS ''" > $DIR_NAME/FP.tsv
+
+
+# STORE holdout set
+deepdive sql "COPY (SELECT * FROM genepheno_causation_labels gal WHERE gal.version = $VERSION) TO STDOUT WITH NULL AS ''" > $DIR_NAME/holdout_set.tsv
+
+# STORE a path to sentences_input used + count
+echo '/dfs/scratch0/genomics-data/sentences_input_v0.sql' > $DIR_NAME/input_data
+deepdive sql "COPY (SELECT count(*) FROM sentences_input) TO STDOUT WITH NULL AS ''" >> $DIR_NAME/input_data
+
+
+
+
