4e71caf9a9738ddfb83dc67c7671652b3122fa27
Johannes Birgmeier <jbirgmei@stanford.edu>
2016-02-09 19:07:29 -0800

    Better charite supervision set

--- onto/create_charite_supervision_data.sh
+++ onto/create_charite_supervision_data.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
 cat manual/charite_all_sources_gene_to_pheno.tsv | cut -f 1,2 | sort | uniq > data/charite_supervision_ensgene.tsv
-cat manual/charite_all_sources_gene_to_disease_to_pheno.tsv | cut -f 1,3 | sort | uniq >> data/charite_supervision_ensgene.tsv
+cat manual/charite_all_sources_gene_to_disease_to_pheno.tsv | awk '{OFS="\t"; print $3, $2}' | sort | uniq >> data/charite_supervision_ensgene.tsv
 join -1 2 -2 2 -t $'\t' \
   <(cat manual/charite_all_sources_gene_to_disease_to_pheno.tsv | 
     cut -f 2,3 | sort | uniq | sort -k2,2) \
   <(cat manual/phenotypic_series_to_omim.tsv | 
     sort | uniq | sort -k2,2) |
-  cut -f 2,3 | sort | uniq >> data/charite_supervision_ensgene.tsv
+  awk '{OFS="\t"; print $3, $2}' | sort | uniq >> data/charite_supervision_ensgene.tsv
 join -1 2 -2 1 -t $'\t' \
   <(cat data/charite_supervision_ensgene.tsv | sort -k2,2) \
   <(cat data/ensembl_genes.tsv | 
