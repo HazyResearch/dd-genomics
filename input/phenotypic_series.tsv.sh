2ec9deb73711a6a6c2c0329953de79f9bae51723
Johannes Birgmeier <jbirgmei@stanford.edu>
2016-02-10 19:04:24 -0800

    Got rid of cancer diseases in a better way

---
+++ input/phenotypic_series.tsv.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+if [ -z $GDD_HOME ]
+then
+  echo "Set GDD_HOME!!" >> /dev/stderr
+  exit 1
+fi
+
+cat $GDD_HOME/onto/manual/phenotypic_series.tsv | $GDD_HOME/code/create_allowed_diseases_list.py
