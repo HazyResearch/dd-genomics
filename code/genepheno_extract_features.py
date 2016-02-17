5a223a38866728ce69760167516c3aef3f9ef585
Alexander Ratner <ajratner@gmail.com>
2016-02-16 20:57:26 -0800

    Killed off DDLIB!  Made run_main_tsv work with generators

--- code/genepheno_extract_features.py
+++ code/genepheno_extract_features.py
@@ -27,20 +27,6 @@ parser = util.RowParser([
 
 Feature = namedtuple('Feature', ['doc_id', 'section_id', 'relation_id', 'name'])
 
-
-def get_features_for_candidate_ddlib(row):
-  """Extract features for candidate mention- both generic ones from ddlib & custom features"""
-  features = []
-  f = Feature(doc_id=row.doc_id, section_id=row.section_id, relation_id=row.relation_id, name=None)
-  dds = util.create_ddlib_sentence(row)
-
-  # (1) GENERIC FEATURES from ddlib
-  gene_span = ddlib.Span(begin_word_id=row.gene_wordidxs[0], length=len(row.gene_wordidxs))
-  pheno_span = ddlib.Span(begin_word_id=row.pheno_wordidxs[0], length=len(row.pheno_wordidxs))
-  features += [f._replace(name=feat) \
-                    for feat in ddlib.get_generic_features_relation(dds, gene_span, pheno_span)]
-  return features
-
 CoreNLPSentence = namedtuple('CoreNLPSentence', 'words, lemmas, poses, ners, dep_paths, dep_parents')
 
 def get_features_for_candidate_treedlib(r):
@@ -53,23 +39,8 @@ def get_features_for_candidate_treedlib(r):
   xt = corenlp_to_xmltree(s)
 
   # Get features
-  #for feature in get_relation_binning_features(xt.root, r.gene_wordidxs, r.pheno_wordidxs):
   for feature in get_relation_features(xt.root, r.gene_wordidxs, r.pheno_wordidxs):
-    features += [f._replace(name=feature)]
-  return features
-
-# Helper for loading in manually defined keywords
-onto_path = lambda p : '%s/onto/%s' % (os.environ['GDD_HOME'], p)
-
-def get_features_for_candidate(row):
-  return get_features_for_candidate_treedlib(row)
-  """
-  if row.num_gene_candidates >= 2 and row.num_pheno_candidates >= 2:
-    return get_features_for_candidate_treedlib(row)
-  else:
-    return get_features_for_candidate_ddlib(row)
-  """
+    yield f._replace(name=feature)
 
 if __name__ == '__main__':
-  ddlib.load_dictionary(onto_path("manual/genepheno_keywords.txt"), dict_id="gp_relation_kws")
-  util.run_main_tsv(row_parser=parser.parse_tsv_row, row_fn=get_features_for_candidate)
+  util.run_main_tsv(row_parser=parser.parse_tsv_row, row_fn=get_features_for_candidate_treedlib)
